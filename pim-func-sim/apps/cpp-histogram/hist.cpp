a28d9ca5c84ab3fc6ade102a92f92d79b430d8a6
hugoabbot <drt3sm@virginia.edu>
2024-06-01 22:42:34 -0400

    using redsumranged and splitting input when greater tahn avaiablebits

--- pim-func-sim/apps/cpp-histogram/hist.cpp
+++ pim-func-sim/apps/cpp-histogram/hist.cpp
@@ -10,6 +10,7 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <cmath>
 #include <cstring>
 #if defined(_OPENMP)
 #include <omp.h>
@@ -82,176 +83,7 @@ struct Params getInputParams(int argc, char **argv)
   return p;
 }
 
-// void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<int> &red, std::vector<int> &green, std::vector<int> &blue) 
-// {
-//   unsigned bitsPerElement = sizeof(int) * 8;
-//   PimObjId imgObj = pimAlloc(PIM_ALLOC_AUTO, imgdata_bytes, bitsPerElement, PIM_INT32);
-//   if (imgObj == -1)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   PimObjId keyObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
-//   if (keyObj == -1)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   PimObjId redMaskObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
-//   if (redMaskObj == -1)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   PimObjId greenMaskObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
-//   if (greenMaskObj == -1)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   PimObjId blueMaskObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
-//   if (blueMaskObj == -1)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   PimObjId tempResultObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
-//   if (tempResultObj == -1)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-
-//   PimStatus status = pimCopyHostToDevice((void *) imgData.data(), imgObj);
-//   if (status != PIM_OK)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-
-//   // Start Mask setup
-//   std::vector<int> redMask(imgdata_bytes, 0), greenMask(imgdata_bytes, 0), blueMask(imgdata_bytes, 0);
-//   for (int i = 0; i < imgdata_bytes; i+=3) 
-//   {
-//     redMask[i + 2] = 1;
-//     greenMask[i + 1] = 1;
-//     blueMask[i] = 1;
-//   }
-//   status = pimCopyHostToDevice((void *) redMask.data(), redMaskObj);
-//   if (status != PIM_OK)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   status = pimCopyHostToDevice((void *) greenMask.data(), greenMaskObj);
-//   if (status != PIM_OK)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   status = pimCopyHostToDevice((void *) blueMask.data(), blueMaskObj);
-//   if (status != PIM_OK)
-//   {
-//     std::cout << "Abort" << std::endl;
-//     return;
-//   }
-//   // End Mask setup
-//   std::vector<int> temp(imgdata_bytes, 0);
-
-//   for (int i = 0; i < 256; ++i) 
-//   {
-//     status = pimBroadcast(keyObj, i);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-
-//     status = pimEQ(keyObj, imgObj, keyObj);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-//     // status = pimCopyDeviceToHost(keyObj, (void *) temp.data());
-//     // for (int i = 0; i < imgdata_bytes; ++i) {
-//     //   std::cout << temp[i] << std::endl;
-//     // }
-
-//     status = pimAnd(keyObj, blueMaskObj, tempResultObj);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-//     // status = pimPopCount(tempResultObj, tempResultObj);
-//     // if (status != PIM_OK)
-//     // {
-//     //   std::cout << "Abort" << std::endl;
-//     //   return;  
-//     // }
-//     // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
-//     // blue[i] = temp[0];
-//     status = pimRedSum(tempResultObj, &blue[i]);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-
-//     status = pimAnd(keyObj, greenMaskObj, tempResultObj);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-//     // status = pimPopCount(tempResultObj, tempResultObj);
-//     // if (status != PIM_OK)
-//     // {
-//     //   std::cout << "Abort" << std::endl;
-//     //   return;  
-//     // }
-//     // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
-//     // green[i] = temp[0];
-//     status = pimRedSum(tempResultObj, &green[i]);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-
-//     status = pimAnd(keyObj, redMaskObj, tempResultObj);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-//     // status = pimPopCount(tempResultObj, tempResultObj);
-//     // if (status != PIM_OK)
-//     // {
-//     //   std::cout << "Abort" << std::endl;
-//     //   return;  
-//     // }
-//     // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
-//     // blue[i] = temp[0];
-//     status = pimRedSum(tempResultObj, &red[i]);
-//     if (status != PIM_OK)
-//     {
-//       std::cout << "Abort" << std::endl;
-//       return;
-//     }
-//     std::cout << "Blue: '" << blue[i] << "' | Green: '" << green[i] << "' | Red: '" << red[i] << "'" << std::endl;
-//     //temp.clear();
-//   }
-//   pimFree(imgObj);
-//   pimFree(keyObj);
-//   pimFree(blueMaskObj);
-//   pimFree(greenMaskObj);
-//   pimFree(redMaskObj);
-//   pimFree(tempResultObj);
-// }
-
-void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<int> &counter) 
+void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<int> &red, std::vector<int> &green, std::vector<int> &blue) 
 {
   unsigned bitsPerElement = sizeof(int) * 8;
   PimObjId imgObj = pimAlloc(PIM_ALLOC_AUTO, imgdata_bytes, bitsPerElement, PIM_INT32);
@@ -266,12 +98,30 @@ void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<i
     std::cout << "Abort" << std::endl;
     return;
   }
-  // PimObjId tempResultObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
-  // if (tempResultObj == -1)
-  // {
-  //   std::cout << "Abort" << std::endl;
-  //   return;
-  // }
+  PimObjId redMaskObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
+  if (redMaskObj == -1)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
+  PimObjId greenMaskObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
+  if (greenMaskObj == -1)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
+  PimObjId blueMaskObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
+  if (blueMaskObj == -1)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
+  PimObjId tempResultObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
+  if (tempResultObj == -1)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
 
   PimStatus status = pimCopyHostToDevice((void *) imgData.data(), imgObj);
   if (status != PIM_OK)
@@ -280,9 +130,39 @@ void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<i
     return;
   }
 
+  // Start Mask setup
+  std::vector<int> redMask(imgdata_bytes, 0), greenMask(imgdata_bytes, 0), blueMask(imgdata_bytes, 0);
+  for (int i = 0; i < imgdata_bytes; i+=3) 
+  {
+    redMask[i + 2] = 1;
+    greenMask[i + 1] = 1;
+    blueMask[i] = 1;
+  }
+  status = pimCopyHostToDevice((void *) redMask.data(), redMaskObj);
+  if (status != PIM_OK)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
+  status = pimCopyHostToDevice((void *) greenMask.data(), greenMaskObj);
+  if (status != PIM_OK)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
+  status = pimCopyHostToDevice((void *) blueMask.data(), blueMaskObj);
+  if (status != PIM_OK)
+  {
+    std::cout << "Abort" << std::endl;
+    return;
+  }
+  // End Mask setup
+  std::vector<int> temp(imgdata_bytes, 0);
+
   for (int i = 0; i < 256; ++i) 
   {
     status = pimBroadcast(keyObj, i);
+    int temp;
     if (status != PIM_OK)
     {
       std::cout << "Abort" << std::endl;
@@ -290,6 +170,17 @@ void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<i
     }
 
     status = pimEQ(keyObj, imgObj, keyObj);
+    if (status != PIM_OK)
+    {
+      std::cout << "Abort" << std::endl;
+      return;
+    }
+    // status = pimCopyDeviceToHost(keyObj, (void *) temp.data());
+    // for (int i = 0; i < imgdata_bytes; ++i) {
+    //   std::cout << temp[i] << std::endl;
+    // }
+
+    status = pimAnd(keyObj, blueMaskObj, tempResultObj);
     if (status != PIM_OK)
     {
       std::cout << "Abort" << std::endl;
@@ -303,17 +194,129 @@ void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<i
     // }
     // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
     // blue[i] = temp[0];
-    status = pimRedSum(keyObj, &counter[i]);
+    temp = blue[i];
+    status = pimRedSumRanged(tempResultObj, 0, imgdata_bytes, &blue[i]);
     if (status != PIM_OK)
     {
       std::cout << "Abort" << std::endl;
       return;
     }
-    std::cout << "i value: '" << i << "' = " << counter[i] << std::endl;
+    blue[i] += temp;
+
+    status = pimAnd(keyObj, greenMaskObj, tempResultObj);
+    if (status != PIM_OK)
+    {
+      std::cout << "Abort" << std::endl;
+      return;
+    }
+    // status = pimPopCount(tempResultObj, tempResultObj);
+    // if (status != PIM_OK)
+    // {
+    //   std::cout << "Abort" << std::endl;
+    //   return;  
+    // }
+    // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
+    // green[i] = temp[0];
+    temp = green[i];
+    status = pimRedSumRanged(tempResultObj, 0, imgdata_bytes, &green[i]);
+    if (status != PIM_OK)
+    {
+      std::cout << "Abort" << std::endl;
+      return;
+    }
+    green[i] += temp;
+
+    status = pimAnd(keyObj, redMaskObj, tempResultObj);
+    if (status != PIM_OK)
+    {
+      std::cout << "Abort" << std::endl;
+      return;
+    }
+    // status = pimPopCount(tempResultObj, tempResultObj);
+    // if (status != PIM_OK)
+    // {
+    //   std::cout << "Abort" << std::endl;
+    //   return;  
+    // }
+    // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
+    // blue[i] = temp[0];
+    temp = red[i];
+    status = pimRedSumRanged(tempResultObj, 0, imgdata_bytes, &red[i]);
+    if (status != PIM_OK)
+    {
+      std::cout << "Abort" << std::endl;
+      return;
+    }
+    red[i] += temp;
+    std::cout << "Blue: '" << blue[i] << "' | Green: '" << green[i] << "' | Red: '" << red[i] << "'" << std::endl;
+    //temp.clear();
   }
   pimFree(imgObj);
   pimFree(keyObj);
+  pimFree(blueMaskObj);
+  pimFree(greenMaskObj);
+  pimFree(redMaskObj);
+  pimFree(tempResultObj);
 }
+
+// void histogram(int imgdata_bytes, const std::vector<int> &imgData, std::vector<int> &counter) 
+// {
+//   unsigned bitsPerElement = sizeof(int) * 8;
+//   PimObjId imgObj = pimAlloc(PIM_ALLOC_AUTO, imgdata_bytes, bitsPerElement, PIM_INT32);
+//   if (imgObj == -1)
+//   {
+//     std::cout << "Abort" << std::endl;
+//     return;
+//   }
+//   PimObjId keyObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_INT32);
+//   if (keyObj == -1)
+//   {
+//     std::cout << "Abort" << std::endl;
+//     return;
+//   }
+
+//   PimStatus status = pimCopyHostToDevice((void *) imgData.data(), imgObj);
+//   if (status != PIM_OK)
+//   {
+//     std::cout << "Abort" << std::endl;
+//     return;
+//   }
+
+//   std::vector<int> temp(imgdata_bytes);
+
+//   for (int i = 0; i < 256; ++i) 
+//   {
+//     status = pimBroadcast(keyObj, i);
+//     if (status != PIM_OK)
+//     {
+//       std::cout << "Abort" << std::endl;
+//       return;
+//     }
+//     status = pimEQ(keyObj, imgObj, keyObj);
+//     if (status != PIM_OK)
+//     {
+//       std::cout << "Abort" << std::endl;
+//       return;
+//     }
+//     // status = pimPopCount(tempResultObj, tempResultObj);
+//     // if (status != PIM_OK)
+//     // {
+//     //   std::cout << "Abort" << std::endl;
+//     //   return;  
+//     // }
+//     // status = pimCopyDeviceToHost(tempResultObj, (void *) temp.data());
+//     // blue[i] = temp[0];
+//     status = pimRedSum(keyObj, &counter[i]);
+//     if (status != PIM_OK)
+//     {
+//       std::cout << "Abort" << std::endl;
+//       return;
+//     }
+//     std::cout << "i value: '" << i << "' = " << counter[i] << std::endl;
+//   }
+//   pimFree(imgObj);
+//   pimFree(keyObj);
+// }
 
 int main(int argc, char *argv[])
 {
@@ -367,26 +370,51 @@ int main(int argc, char *argv[])
   std::vector<int> red(256, 0), green(256, 0), blue(256, 0);
 
   std::vector<uint8_t> imgData(fdata + *data_pos, fdata + finfo.st_size);
-  // std::vector<int> imgDataToInt;
+  std::vector<int> imgDataToInt;
 
-  // for (int i = 0; i < imgdata_bytes; ++i) {
-  //   imgDataToInt.push_back(static_cast<int> (imgData[i]));
-  // }
-
-  std::vector<int> blueImgData;
-  std::vector<int> greenImgData;
-  std::vector<int> redImgData;
-
-  for (int i = 0; i < imgdata_bytes; i+=3) {
-    // imgDataToInt.push_back(static_cast<int>(val));
-    blueImgData.push_back(static_cast<int> (imgData[i]));
-    greenImgData.push_back(static_cast<int> (imgData[i+1]));
-    redImgData.push_back(static_cast<int> (imgData[i+2]));
+  for (int i = 0; i < imgdata_bytes; ++i) {
+    imgDataToInt.push_back(static_cast<int> (imgData[i]));
   }
 
-  histogram(imgdata_bytes/3, blueImgData, blue);
-  histogram(imgdata_bytes/3, greenImgData, green);
-  histogram(imgdata_bytes/3, redImgData, red);
+  // std::vector<int> blueImgData;
+  // std::vector<int> greenImgData;
+  // std::vector<int> redImgData;
+
+  // for (int i = 0; i < imgdata_bytes; i+=3) {
+  //   // imgDataToInt.push_back(static_cast<int>(val));
+  //   blueImgData.push_back(static_cast<int> (imgData[i]));
+  //   greenImgData.push_back(static_cast<int> (imgData[i+1]));
+  //   redImgData.push_back(static_cast<int> (imgData[i+2]));
+  // }
+
+  uint64_t numCol = 8192, numRow = 8192, numCore = 4096;
+  uint64_t totalAvailableBits = numCol * numRow * numCore;
+  uint64_t requiredBitsforImage = (imgdata_bytes * 32) + 32;
+  int numItr = std::ceil(static_cast<double>(requiredBitsforImage) / totalAvailableBits);
+
+  if (numItr == 1)
+  {
+    histogram(imgdata_bytes, imgDataToInt, red, green, blue);
+  }
+  else
+  {
+    // TODO
+    uint64_t bitsPerChunk = totalAvailableBits - 32;
+    uint64_t bytesPerChunk = bitsPerChunk / 32;
+    for (int itr = 0; itr < numItr; ++itr)
+    {
+      uint64_t startByte = itr * totalAvailableBits;
+      uint64_t endByte = std::min(startByte + (totalAvailableBits * 32), static_cast<uint64_t>(imgdata_bytes));
+      uint64_t chunkSize = endByte - startByte;
+
+      std::vector<int> imgDataChunk(imgDataToInt.begin() + startByte, imgDataToInt.begin() + endByte);
+      histogram(chunkSize, imgDataChunk, red, green, blue);
+    }
+  }
+
+  // histogram(imgdata_bytes/3, blueImgData, blue);
+  // histogram(imgdata_bytes/3, greenImgData, green);
+  // histogram(imgdata_bytes/3, redImgData, red);
 
   // std::vector<int> slicing;
   // for (int i = 0; i < imgdata_bytes; ++i) {
