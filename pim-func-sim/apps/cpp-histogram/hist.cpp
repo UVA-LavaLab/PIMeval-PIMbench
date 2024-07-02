4c6b2f73fefc2e8ebb8c60b5cc24aa51b29217a8
hugoabbot <drt3sm@virginia.edu>
2024-07-02 10:17:22 -0400

    improve readability of histogram code

--- pim-func-sim/apps/cpp-histogram/hist.cpp
+++ pim-func-sim/apps/cpp-histogram/hist.cpp
@@ -19,9 +19,6 @@
 #include "../util.h"
 #include "libpimsim.h"
 
-#define IMG_DATA_OFFSET_POS 10
-#define BITS_PER_PIXEL_POS 28
-
 using namespace std;
 
 // Params ---------------------------------------------------------------------
@@ -85,6 +82,7 @@ struct Params getInputParams(int argc, char **argv)
 
 void orderData(std::vector<uint8_t> &orderedImgData, const std::vector<uint8_t> imgData, uint64_t idxBegin, uint64_t idxEnd)
 {
+  // Splitting apart individual pixel values by grouping image data vector by color channels, going blue, green, then red
   for (int i = 0; i < 3; ++i)
   {
     for (uint64_t j = idxBegin; j < idxEnd; j+=3)
@@ -94,10 +92,10 @@ void orderData(std::vector<uint8_t> &orderedImgData, const std::vector<uint8_t> 
   }
 }
 
-void histogram(uint64_t imgdata_bytes, const std::vector<uint8_t> &imgData, std::vector<uint64_t> &redCount, std::vector<uint64_t> &greenCount, std::vector<uint64_t> &blueCount) 
+void histogram(uint64_t imgDataBytes, const std::vector<uint8_t> &imgData, std::vector<uint64_t> &redCount, std::vector<uint64_t> &greenCount, std::vector<uint64_t> &blueCount) 
 {
   unsigned bitsPerElement = sizeof(uint8_t) * 8;
-  PimObjId imgObj = pimAlloc(PIM_ALLOC_AUTO, imgdata_bytes, bitsPerElement, PIM_UINT8);
+  PimObjId imgObj = pimAlloc(PIM_ALLOC_AUTO, imgDataBytes, bitsPerElement, PIM_UINT8);
   assert(imgObj != -1);
   PimObjId tempObj = pimAllocAssociated(bitsPerElement, imgObj, PIM_UINT8);
   assert(tempObj != -1);
@@ -105,27 +103,27 @@ void histogram(uint64_t imgdata_bytes, const std::vector<uint8_t> &imgData, std:
   PimStatus status = pimCopyHostToDevice((void *) imgData.data(), imgObj);
   assert(status == PIM_OK);
 
-  uint64_t temp;
+  uint64_t prevCount; // Needed when image data can't fit in one PIM object, multiple passes are needed
 
   for (uint64_t i = 0; i < 256; ++i) 
   {
     status = pimEQScalar(imgObj, tempObj, i);
     assert(status == PIM_OK);
 
-    temp = blueCount[i];
-    status = pimRedSumRangedUInt(tempObj, 0, imgdata_bytes / 3, &blueCount[i]);
+    prevCount = blueCount[i];
+    status = pimRedSumRangedUInt(tempObj, 0, imgDataBytes / 3, &blueCount[i]);
     assert(status == PIM_OK);
-    blueCount[i] += temp;
+    blueCount[i] += prevCount;
 
-    temp = greenCount[i];
-    status = pimRedSumRangedUInt(tempObj, imgdata_bytes / 3, imgdata_bytes / 3 * 2, &greenCount[i]);
+    prevCount = greenCount[i];
+    status = pimRedSumRangedUInt(tempObj, imgDataBytes / 3, imgDataBytes / 3 * 2, &greenCount[i]);
     assert(status == PIM_OK);
-    greenCount[i] += temp;
+    greenCount[i] += prevCount;
 
-    temp = redCount[i];
-    status = pimRedSumRangedUInt(tempObj, imgdata_bytes / 3 * 2, imgdata_bytes, &redCount[i]);
+    prevCount = redCount[i];
+    status = pimRedSumRangedUInt(tempObj, imgDataBytes / 3 * 2, imgDataBytes, &redCount[i]);
     assert(status == PIM_OK);
-    redCount[i] += temp;
+    redCount[i] += prevCount;
   }
   pimFree(imgObj);
   pimFree(tempObj);
@@ -138,40 +136,51 @@ int main(int argc, char *argv[])
   std::string fn = params.inputFile;
   std::cout << "Input file : '" << fn << "'" << std::endl;
   int fd;
-  uint64_t imgdata_bytes;
+  uint64_t imgDataBytes;
+  int numChannels;
+  int imgDataOffsetPosition;
   struct stat finfo;
   char* fdata;
-  unsigned short* data_pos;
+  unsigned short* dataPos;
 
   // Start data parsing
   if (!fn.substr(fn.find_last_of(".") + 1).compare("bmp") == 0)
   {
-    // TODO: Assuming inputs will be 24-bit .bmp files
+    // TODO: reading in other types of input files
     std::cout << "Need work reading in other file types" << std::endl;
     return 1;
   }
-  fd = open(params.inputFile.c_str(), O_RDONLY);
-  if (fd < 0) 
+  else
   {
-    perror("Failed to open input file, or file doesn't exist");
-    return 1;
-  }
-  if (fstat(fd, &finfo) < 0) 
-  {
-    perror("Failed to get file info");
-    return 1;
-  }
-  fdata = static_cast<char *>(mmap(0, finfo.st_size + 1, PROT_READ, MAP_PRIVATE, fd, 0));
-  if (fdata == 0) 
-  {
-    perror("Failed to memory map the file");
-    return 1;
-  }
+    // Assuming input will be 24-bit .bmp files
+    fd = open(params.inputFile.c_str(), O_RDONLY);
+    if (fd < 0) 
+    {
+      perror("Failed to open input file, or file doesn't exist");
+      return 1;
+    }
+    if (fstat(fd, &finfo) < 0) 
+    {
+      perror("Failed to get file info");
+      return 1;
+    }
+    fdata = static_cast<char *>(mmap(0, finfo.st_size + 1, PROT_READ, MAP_PRIVATE, fd, 0));
+    if (fdata == 0) 
+    {
+      perror("Failed to memory map the file");
+      return 1;
+    }
 
-  data_pos = (unsigned short *)(&(fdata[IMG_DATA_OFFSET_POS]));
-  imgdata_bytes = static_cast<uint64_t> (finfo.st_size) - static_cast<uint64_t>(*(data_pos));
-  printf("This file has %ld bytes of image data, %ld pixels\n", imgdata_bytes, imgdata_bytes / 3);
+    numChannels = 3; // Red, green, and blue color channels
+    imgDataOffsetPosition = 10; // Start of image data, ignoring unneeded header data and info
+    // Defined according to the assumed input file structure given
+
+    dataPos = (unsigned short *)(&(fdata[imgDataOffsetPosition]));
+    imgDataBytes = static_cast<uint64_t> (finfo.st_size) - static_cast<uint64_t>(*(dataPos));
+  }
   // End data parsing
+
+  printf("This file has %ld bytes of image data, %ld pixels\n", imgDataBytes, imgDataBytes / numChannels);
 
   if (!createDevice(params.configFile))
   {
@@ -179,20 +188,19 @@ int main(int argc, char *argv[])
   }
 
   std::vector<uint64_t> redCount(256, 0), greenCount(256, 0), blueCount(256, 0);
-  std::vector<uint8_t> imgData(fdata + *data_pos, fdata + finfo.st_size);
-
+  std::vector<uint8_t> imgData(fdata + *dataPos, fdata + finfo.st_size);
   std::vector<uint8_t> orderedImgData;
 
   uint64_t numCol = 8192, numRow = 8192, numCore = 4096;
   uint64_t totalAvailableBits = numCol * numRow * numCore;
-  uint64_t requiredBitsforImage = ((imgdata_bytes * 32) + 32);
+  uint64_t requiredBitsforImage = ((imgDataBytes * 32) + 32);
   int numItr = std::ceil(static_cast<double> (requiredBitsforImage) / totalAvailableBits);
   std::cout << "Required iterations for image: " << numItr << std::endl;
 
   if (numItr == 1)
   {
-    orderData(orderedImgData, imgData, 0, imgdata_bytes);
-    histogram(imgdata_bytes, orderedImgData, redCount, greenCount, blueCount);
+    orderData(orderedImgData, imgData, 0, imgDataBytes);
+    histogram(imgDataBytes, orderedImgData, redCount, greenCount, blueCount);
   }
   else
   {
@@ -202,13 +210,15 @@ int main(int argc, char *argv[])
     for (int itr = 0; itr < numItr; ++itr)
     {
       uint64_t startByte = itr * bytesPerChunk;
-      uint64_t endByte = std::min(startByte + bytesPerChunk, imgdata_bytes);
+      uint64_t endByte = std::min(startByte + bytesPerChunk, imgDataBytes);
       uint64_t chunkSize = endByte - startByte;
 
       std::vector<uint8_t> imgDataChunk(imgData.begin() + startByte, imgData.begin() + endByte);
       orderData(orderedImgData, imgDataChunk, startByte, endByte);
 
       histogram(chunkSize, orderedImgData, redCount, greenCount, blueCount);
+
+      orderedImgData.clear();
     }
   }
 
@@ -223,7 +233,7 @@ int main(int argc, char *argv[])
     memset(&(greenCheck[0]), 0, sizeof(uint64_t) * 256);
     memset(&(blueCheck[0]), 0, sizeof(uint64_t) * 256);
    
-    for (int i=*data_pos; i < finfo.st_size; i+=3) 
+    for (int i=*dataPos; i < finfo.st_size; i+=3) 
     {      
       unsigned char *val = (unsigned char *)&(fdata[i]);
       blueCheck[*val]++;
@@ -239,17 +249,17 @@ int main(int argc, char *argv[])
     {
       if (redCheck[i] != redCount[i]) 
       {
-        std::cout << "Index " << i << " | Wrong answer for red: " << redCount[i] << " (expected " << redCheck[i] << ")" << std::endl;
+        std::cout << "Index " << i << " | Wrong PIM answer for red = " << redCount[i] << " (CPU expected = " << redCheck[i] << ")" << std::endl;
         errorFlag = 1;
       }
       if (greenCheck[i] != greenCount[i]) 
       {
-        std::cout << "Index " << i << " | Wrong answer for green: " << greenCount[i] << " (expected " << greenCheck[i] << ")" << std::endl;
+        std::cout << "Index " << i << " | Wrong PIM answer for green = " << greenCount[i] << " (CPU expected = " << greenCheck[i] << ")" << std::endl;
         errorFlag = 1;
       }
       if (blueCheck[i] != blueCount[i]) 
       {
-        std::cout << "Index " << i << " | Wrong answer for blue: " << blueCount[i] << " (expected " << blueCheck[i] << ")" << std::endl;
+        std::cout << "Index " << i << " | Wrong PIM answer for blue = " << blueCount[i] << " (CPU expected = " << blueCheck[i] << ")" << std::endl;
         errorFlag = 1;
       }
     }
