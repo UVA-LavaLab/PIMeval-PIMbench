645986a8ac40a9b63a7d21a8faa75a37e891cf7f
fasiddique <farzana@virginia.edu>
2024-07-23 18:46:01 -0400

    prefix sum bug fix

--- PIMbench/cpp-prefixsum/prefixsum.cpp
+++ PIMbench/cpp-prefixsum/prefixsum.cpp
@@ -27,7 +27,7 @@ void usage()
   fprintf(stderr,
           "\nUsage:  ./prefixsum.out [options]"
           "\n"
-          "\n    -l    input size (default=65536 elements)"
+          "\n    -l    input size (default=2048 elements)"
           "\n    -c    dramsim config file"
           "\n    -i    input file containing interger vector (default=generates datapoints with random numbers)"
           "\n    -v    t = verifies PIM output with host output. (default=false)"
@@ -37,7 +37,7 @@ void usage()
 struct Params getInputParams(int argc, char **argv)
 {
   struct Params p;
-  p.vectorLength = 65536;
+  p.vectorLength = 2048;
   p.configFile = nullptr;
   p.inputFile = nullptr;
   p.shouldVerify = false;
@@ -72,139 +72,139 @@ struct Params getInputParams(int argc, char **argv)
   return p;
 }
 
-void prefixSum(vector<int>& input, vector<int>& deviceoutput, uint64_t len)
+void prefixSum(vector<int> &input, vector<int> &deviceoutput, uint64_t len)
 {
- unsigned bitsPerElement = sizeof(int) * 8;
- std::vector<PimObjId> temp(len);
- std::vector<PimObjId> acc(len);
+  unsigned bitsPerElement = sizeof(int) * 8;
+  std::vector<PimObjId> temp(len);
+  std::vector<PimObjId> acc(len);
 
- PimObjId inputObj = pimAlloc(PIM_ALLOC_AUTO, len, bitsPerElement, PIM_INT32);
- if (inputObj == -1) 
- {
-   std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
-   return;
- }
+  PimObjId inputObj = pimAlloc(PIM_ALLOC_AUTO, len, bitsPerElement, PIM_INT32);
+  if (inputObj == -1)
+  {
+    std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
+    return;
+  }
 
- PimStatus status = pimCopyHostToDevice((void*)input.data(), inputObj);
- if (status != PIM_OK) 
- {
-   std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
-   return;
- }
- PimObjId tempObj = pimAllocAssociated(bitsPerElement, inputObj, PIM_INT32);
- if (tempObj == -1) 
- {
-   std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
-   return;
- }
- status = pimCopyHostToDevice((void*)input.data(), tempObj);
- if (status != PIM_OK) 
- {
-   std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
-   return;
- }
+  PimStatus status = pimCopyHostToDevice((void *)input.data(), inputObj);
+  if (status != PIM_OK)
+  {
+    std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
+    return;
+  }
+  PimObjId tempObj = pimAllocAssociated(bitsPerElement, inputObj, PIM_INT32);
+  if (tempObj == -1)
+  {
+    std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
+    return;
+  }
+  status = pimCopyHostToDevice((void *)input.data(), tempObj);
+  if (status != PIM_OK)
+  {
+    std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
+    return;
+  }
 
- PimObjId accObj = pimAllocAssociated(bitsPerElement, inputObj, PIM_INT32);
- if (accObj == -1) 
- {
-   std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
-   return;
- }
+  PimObjId accObj = pimAllocAssociated(bitsPerElement, inputObj, PIM_INT32);
+  if (accObj == -1)
+  {
+    std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
+    return;
+  }
 
- status = pimCopyHostToDevice((void*)input.data(), accObj);
- if (status != PIM_OK) 
- {
-   std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
-   return;
+  status = pimCopyHostToDevice((void *)input.data(), accObj);
+  if (status != PIM_OK)
+  {
+    std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
+    return;
+  }
+
+  PimObjId outputObj = pimAllocAssociated(bitsPerElement, inputObj, PIM_INT32);
+  if (outputObj == -1)
+  {
+    std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
+    return;
+  }
+
+  status = pimCopyHostToDevice((void *)deviceoutput.data(), outputObj);
+  if (status != PIM_OK)
+  {
+    std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
+    return;
+  }
+
+  while (len > 0)
+  {
+    pimShiftElementsRight(tempObj);
+    status = pimAdd(tempObj, accObj, accObj);
+    if (status != PIM_OK)
+    {
+      std::cerr << "Abort: Failed to perform PIM addition." << std::endl;
+      return;
+    }
+    len--;
+  }
+
+  status = pimCopyDeviceToHost(accObj, (void *)deviceoutput.data());
+  if (status != PIM_OK)
+  {
+    std::cerr << "Abort: Failed to copy prefix sum result from PIM." << std::endl;
+    return;
+  }
+
+  // Clean up PIM objects
+  pimFree(accObj);
+  pimFree(tempObj);
+  pimFree(inputObj);
+  pimFree(outputObj);
 }
 
-PimObjId outputObj = pimAllocAssociated(bitsPerElement, inputObj, PIM_INT32);
-if (outputObj == -1) 
-{
-  std::cerr << "Abort: Failed to allocate memory on PIM." << std::endl;
-  return;
-}
-
-status = pimCopyHostToDevice((void*)deviceoutput.data(), outputObj);
-if (status != PIM_OK) 
-{
-  std::cerr << "Abort: Failed to copy data to PIM." << std::endl;
-  return;
-}
-
-while(len>0)
-{
- pimShiftElementsRight(tempObj);
- status = pimAdd(tempObj, accObj, accObj);
- if (status != PIM_OK) 
- {
-   std::cerr << "Abort: Failed to perform PIM addition." << std::endl;
-   return;
- }
-    len --;  
-}
-
-status = pimCopyDeviceToHost(accObj, (void*)deviceoutput.data());
-if (status != PIM_OK)
-{
-  std::cerr << "Abort: Failed to copy prefix sum result from PIM." << std::endl;
-  return;
-}
-
-// Clean up PIM objects
-pimFree(accObj);
-pimFree(tempObj);
-pimFree(inputObj);
-pimFree(outputObj);
-}
-
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
   struct Params params = getInputParams(argc, argv);
-  int range = std::stoi(argv[1]);
-  vector<int> input; 
-  if(params.inputFile == nullptr)
+  vector<int> input;
+  if (params.inputFile == nullptr)
   {
-    getVector(range, input);
-  } 
-   else 
-   {
-     //TODO(read from file)
-   } 
+    getVector(params.vectorLength, input);
+  }
+  else
+  {
+    std::cout << "Reading from input file is not implemented yet." << std::endl;
+    return 1;
+  }
 
-int len=input.size();
-vector<int> deviceoutput;
-vector<int> hostoutput(len);
+  int len = input.size();
+  vector<int> deviceoutput;
+  vector<int> hostoutput(len);
 
-for (int i = 0; i < input.size(); i++) 
-{
-  deviceoutput.push_back(0);
-}
+  for (int i = 0; i < input.size(); i++)
+  {
+    deviceoutput.push_back(0);
+  }
 
-hostoutput[0]=input[0];
-for (int i = 0; i < input.size(); i++) 
-{
-  hostoutput[i+1] = hostoutput[i]+ input[i+1]; 
-}
+  hostoutput[0] = input[0];
+  for (int i = 0; i < input.size(); i++)
+  {
+    hostoutput[i + 1] = hostoutput[i] + input[i + 1];
+  }
 
-if (!createDevice(params.configFile)) return 1;
-prefixSum(input, deviceoutput, len);
+  if (!createDevice(params.configFile))
+    return 1;
+  prefixSum(input, deviceoutput, len);
 
-// Verification of Results hostresults vs deviceresults
-if (params.shouldVerify) 
-{
+  // Verification of Results hostresults vs deviceresults
+  if (params.shouldVerify)
+  {
     // verify result
-  #pragma omp parallel for
-  for (unsigned i = 0; i<len; ++i)
-  {
-    if(hostoutput[i]!= deviceoutput[i])
+#pragma omp parallel for
+    for (unsigned i = 0; i < len; ++i)
     {
-      std::cout << "Wrong answer for Prefixsum: " << hostoutput[i] << " != " << deviceoutput[i] << std::endl;
+      if (hostoutput[i] != deviceoutput[i])
+      {
+        std::cout << "Wrong answer for Prefixsum: " << hostoutput[i] << " != " << deviceoutput[i] << std::endl;
+      }
     }
   }
-}
 
-pimShowStats();
-return 0;
-
+  pimShowStats();
+  return 0;
 }
