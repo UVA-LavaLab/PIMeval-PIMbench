a4be5165a7439c27324d6e8d0e44842dddccc0b8
Morteza <morteza.brothers@gmail.com>
2024-08-03 20:24:06 -0400

    use adjacency list for graph representation to improve the memory footprint

--- PIMbench/cpp-triangle-count/PIM/tc.cpp
+++ PIMbench/cpp-triangle-count/PIM/tc.cpp
@@ -13,6 +13,7 @@
 #include <unordered_set>
 #include <sstream>
 #include <getopt.h>
+#include <unordered_map>
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
@@ -21,8 +22,11 @@
 #include "libpimeval.h"
 
 #define DEBUG 0
-
 #define BITS_PER_INT 32
+
+#define ADJ_MATRIX 0
+#define ADJ_LIST 1
+#define INPUT_MODE ADJ_LIST
 
 typedef uint32_t UINT32;
 
@@ -311,7 +315,7 @@ int run_rowmaxusage_opt(const vector<vector<bool>>& adjMatrix, const vector<vect
 
         }
         if (i % step == 0) {
-            std::cout << "run_rowmaxusage: Progress: " << (i * 100 / V) << "\% rows completed." << std::endl;
+            std::cout << "run_adjmatrix: Progress: " << (i * 100 / V) << "\% rows completed." << std::endl;
         }
     }
     cout << "Host time (for loop): " << std::fixed << std::setprecision(3) << host_time_forloop << " ms." << endl;
@@ -321,7 +325,7 @@ int run_rowmaxusage_opt(const vector<vector<bool>>& adjMatrix, const vector<vect
     return count / 6;
 }
 
-int run_rowmaxusage(const vector<vector<bool>>& adjMatrix, const vector<vector<UINT32>>& bitAdjMatrix, uint64_t words_per_device) {
+int run_adjmatrix(const vector<vector<bool>>& adjMatrix, const vector<vector<UINT32>>& bitAdjMatrix, uint64_t words_per_device) {
     uint64_t operandsCount = 4; // src1, src2, dst, popCountSrc
     uint64_t operandMaxNumberOfWords = words_per_device / operandsCount;
     int count = 0;
@@ -368,14 +372,74 @@ int run_rowmaxusage(const vector<vector<bool>>& adjMatrix, const vector<vector<U
 
         }
         if (i % step == 0) {
-            std::cout << "run_rowmaxusage: Progress: " << (i * 100 / V) << "\% rows completed." << std::endl;
+            std::cout << "Progress: " << (i * 100 / V) << "\% rows completed." << std::endl;
         }
     }
     // Each triangle is counted 6 times (once at each vertex), so divide the count by 6
     return count / 6;
 }
 
-int cpuTriangleCount(const vector<vector<bool>>& adjMatrix) {
+// Function to convert the adjacency list of a node to bitmap format
+void convertToBitMap(const unordered_set<int>& neighbors, int start, vector<uint32_t>& bitMap) {
+    for (int neighbor : neighbors) {
+        // Set the corresponding bit for each neighbor
+        bitMap[start + (neighbor / BITS_PER_INT)] |= (1 << (neighbor % BITS_PER_INT));
+    }
+}
+
+int run_adjlist(const unordered_map<int, unordered_set<int>>& adjList, uint64_t words_per_device) {
+    uint64_t operandsCount = 4; // src1, src2, dst, popCountSrc
+    uint64_t operandMaxNumberOfWords = words_per_device / operandsCount;
+    cout << "operandMaxNumberOfWords: " << operandMaxNumberOfWords << endl;
+    int count = 0;
+    int V = adjList.size();
+    uint64_t wordsPerMatrixRow = (V + BITS_PER_INT - 1) / BITS_PER_INT; // Number of 32-bit integers needed per row
+    assert(wordsPerMatrixRow <=  operandMaxNumberOfWords && "Number of vertices cannot exceed (words_per_device / 2)");
+    int oneCount = 0;
+    uint64_t words = 0;
+    int step = V / 10; // Each 10 percent of the total iterations
+    uint16_t iterations = 0;
+    uint32_t i = 0;
+    vector<uint32_t> src1(operandMaxNumberOfWords, 0);
+    vector<uint32_t> src2(operandMaxNumberOfWords, 0);
+    for (auto it_u = adjList.begin(); it_u != adjList.end(); ++it_u) {
+        const auto& u = it_u->first;
+        const auto& neighborsU = it_u->second;
+        for (auto it_v = neighborsU.begin(); it_v != neighborsU.end(); ++it_v) {
+            const auto& neighborsV = adjList.find(*it_v)->second;
+            convertToBitMap(neighborsU, words, src1);
+            convertToBitMap(neighborsV, words, src2);
+            words += wordsPerMatrixRow;
+            bool isLastIteration = (std::next(it_u) == adjList.end()) && (std::next(it_v) == neighborsU.end());
+            if((words + wordsPerMatrixRow > operandMaxNumberOfWords) || (isLastIteration && words > 0)){
+                cout << "-------------itr[" << iterations << "]-------------" << endl;
+                cout << "Number of words that are processed in this iteration: " << words << endl;
+                std::vector<unsigned int> dst(operandMaxNumberOfWords);
+                std::vector<unsigned int> popCountSrc(operandMaxNumberOfWords);
+                int sum = vectorAndPopCntRedSum((uint64_t) words, src1, src2, dst, popCountSrc);
+
+                if(sum < 0)
+                    return -1;
+                
+                words = 0;
+                iterations++;
+                    // Reset the vectors
+                src1.assign(operandMaxNumberOfWords, 0);
+                src2.assign(operandMaxNumberOfWords, 0);
+                dst.clear();
+                count += sum;
+            }
+        }
+        if (i % step == 0) {
+            std::cout << "Progress: " << (i * 100 / V) << "\% rows completed." << std::endl;
+        }
+        i++;
+    }
+    // Each triangle is counted 6 times (once at each vertex), so divide the count by 6
+    return count / 6;
+}
+
+int cpuTrianglesAdjMatrix(const vector<vector<bool>>& adjMatrix) {
     int count = 0;
     int V = adjMatrix.size();
     for (int i = 0; i < V; ++i) {
@@ -388,6 +452,39 @@ int cpuTriangleCount(const vector<vector<bool>>& adjMatrix) {
         }
     }
     return count / 6; // Each triangle is counted 6 times (once at each vertex), so divide the count by 6
+}
+
+// Function to convert edge list to adjacency list
+unordered_map<int, unordered_set<int>> convertToAdjList(const vector<pair<int, int>>& edgeList) {
+    unordered_map<int, unordered_set<int>> adjList;
+
+    for (const auto& edge : edgeList) {
+        int u = edge.first;
+        int v = edge.second;
+        adjList[u].insert(v);
+        adjList[v].insert(u); // Assuming the graph is undirected
+    }
+
+    return adjList;
+}
+
+// Function to count triangles using adjacency list
+int countTrianglesAdjList(const unordered_map<int, unordered_set<int>>& adjList) {
+    int triangleCount = 0;
+
+    for (const auto& [u, neighborsU] : adjList) {
+        for (const int& v : neighborsU) {
+            if (u < v) { // Ensure each triangle is counted once
+                for (const int& w : adjList.at(v)) {
+                    if (v < w && neighborsU.find(w) != neighborsU.end()) {
+                        triangleCount++;
+                    }
+                }
+            }
+        }
+    }
+
+    return triangleCount;
 }
 
 int main(int argc, char** argv) {
@@ -406,12 +503,6 @@ int main(int argc, char** argv) {
         int numNodes = nodes.size();
         cout << "Number of nodes: " << numNodes << endl;
 
-        // Convert edge list to adjacency matrix
-        vector<vector<bool>> adjMatrix = edgeListToAdjMatrix(edgeList, numNodes);
-        cout << "-----------convertToBitwiseAdjMatrix-----------" << endl;
-    
-        vector<vector<UINT32>> bitAdjMatrix = convertToBitwiseAdjMatrix(adjMatrix);
-
         cout << "-----------createDevice-----------" << endl;
         // create device
         if (!createDevice(params.configFile))
@@ -421,20 +512,42 @@ int main(int argc, char** argv) {
         PimDeviceProperties deviceProp;
         PimStatus status = pimGetDeviceProperties(&deviceProp);
         assert(status == PIM_OK);
-
         uint64_t words_per_device = (uint64_t) deviceProp.numRanks * deviceProp.numBankPerRank * deviceProp.numSubarrayPerBank * deviceProp.numRowPerSubarray * deviceProp.numColPerSubarray / BITS_PER_INT;
-        //run simulation
-        cout << "-----------Start running on PIM-----------" << endl;
-        int pimTriCount = run_rowmaxusage(adjMatrix, bitAdjMatrix, words_per_device);
-
-        if (params.shouldVerify){
-            //run on cpu
-            cout << "-----------Triangle Count Verification-----------" << endl;
-            int cpuTriCount = cpuTriangleCount(adjMatrix);
-            if (cpuTriCount != pimTriCount)
-                printf("PIM count (%d) does not match CPU count(%d)\n", pimTriCount, cpuTriCount);
-            else
-                printf("PIM count (%d) matches CPU count(%d)\n", pimTriCount, cpuTriCount);
+        
+        int pimTriCount = 0;
+        if (INPUT_MODE == ADJ_MATRIX) {
+            // Convert edge list to adjacency matrix
+            cout << "-----------convertToAdjMatrix-----------" << endl;
+            vector<vector<bool>> adjMatrix = edgeListToAdjMatrix(edgeList, numNodes);
+            // Convert adjacency matrix to bitwise adjacency matrix
+            cout << "-----------convertToBitwiseAdjMatrix-----------" << endl;
+            vector<vector<UINT32>> bitAdjMatrix = convertToBitwiseAdjMatrix(adjMatrix);
+            cout << "-----------Start running on PIM-----------" << endl;
+            pimTriCount = run_adjmatrix(adjMatrix, bitAdjMatrix, words_per_device);
+            if (params.shouldVerify){
+                //run on cpu
+                cout << "-----------Triangle Count Verification-----------" << endl;
+                int cpuTriCount = cpuTrianglesAdjMatrix(adjMatrix);
+                if (cpuTriCount != pimTriCount)
+                    printf("PIM count (%d) does not match CPU count(%d)\n", pimTriCount, cpuTriCount);
+                else
+                    printf("PIM count (%d) matches CPU count(%d)\n", pimTriCount, cpuTriCount);
+            }
+        } else if (INPUT_MODE == ADJ_LIST) {
+            // Convert edge list to adjacency list
+            cout << "-----------convertToAdjList-----------" << endl;
+            unordered_map<int, unordered_set<int>> adjList = convertToAdjList(edgeList);
+            cout << "-----------Start running on PIM-----------" << endl;
+            pimTriCount = run_adjlist(adjList, words_per_device);
+            if (params.shouldVerify){
+                //run on cpu
+                cout << "-----------Triangle Count Verification-----------" << endl;
+                int cpuTriCount = countTrianglesAdjList(adjList);
+                if (cpuTriCount != pimTriCount)
+                    printf("PIM count (%d) does not match CPU count(%d)\n", pimTriCount, cpuTriCount);
+                else
+                    printf("PIM count (%d) matches CPU count(%d)\n", pimTriCount, cpuTriCount);
+            }
         }
 
         //stats
