cd193f22bc1405559e2ac8f71278bc5d8bfde5ad
Ethan Ermovick <keg9ve@virginia.edu>
2024-10-18 21:52:29 -0400

    string match progress

--- misc-bench/cpp-string-match/baselines/CPU/string-match.cpp
+++ misc-bench/cpp-string-match/baselines/CPU/string-match.cpp
@@ -18,11 +18,14 @@
 
 #include "utilBaselines.h"
 
+constexpr uint8_t CHAR_OFFSET = 5;
+
 // Params ---------------------------------------------------------------------
 typedef struct Params
 {
   uint64_t stringLength;
   uint64_t keyLength;
+  uint64_t numKeys;
   char *inputFile;
 } Params;
 
@@ -33,7 +36,8 @@ void usage()
           "\n"
           "\n    -s    string size (default=2048 elements)"
           "\n    -k    key size (default = 20 elements)"
-          "\n    -i    input file containing string and key (default=generates strings with random characters)"
+          "\n    -n    number of keys (default = 4 keys)"
+          "\n    -i    input file containing string and keys (default=generates strings with random characters)"
           "\n");
 }
 
@@ -42,10 +46,11 @@ struct Params input_params(int argc, char **argv)
   struct Params p;
   p.stringLength = 2048;
   p.keyLength = 20;
+  p.numKeys = 4;
   p.inputFile = nullptr;
 
   int opt;
-  while ((opt = getopt(argc, argv, "h:s:k:i:")) >= 0)
+  while ((opt = getopt(argc, argv, "h:s:k:n:i:")) >= 0)
   {
     switch (opt)
     {
@@ -58,6 +63,9 @@ struct Params input_params(int argc, char **argv)
       break;
     case 'k':
       p.keyLength = strtoull(optarg, NULL, 0);
+      break;
+    case 'n':
+      p.numKeys = strtoull(optarg, NULL, 0);
       break;
     case 'i':
       p.inputFile = optarg;
@@ -74,16 +82,16 @@ struct Params input_params(int argc, char **argv)
 /**
  * @brief cpu string match kernel
  */
-void string_match(string& needle, string& haystack, vector<uint8_t>& matches) {
-  size_t pos = haystack.find(needle, 0);
-
-  if (pos == string::npos) {
-    return;
+void string_match(vector<string>& needles, string& haystack, vector<uint8_t>& matches) {
+  string shifted_haystack;
+  shifted_haystack.reserve(haystack.size());
+  for(uint64_t i=0; i<haystack.size(); ++i) {
+    shifted_haystack.push_back((uint8_t) (haystack[i] + CHAR_OFFSET));
   }
 
-  while (pos != string::npos) {
-      matches[pos] = 1;
-      pos = haystack.find(needle, pos + 1);
+  for(uint64_t i=0; i<needles.size(); ++i) {
+    size_t pos = shifted_haystack.find(needles[i], 0);
+    matches[i] = (uint8_t) (pos != string::npos);
   }
 }
 
@@ -95,6 +103,13 @@ void getString(string& str, uint64_t len) {
   }
 }
 
+void printVec(vector<uint8_t>& vec) {
+  for(auto match : vec) {
+    cout << (unsigned) (match) << ", ";
+  }
+  cout << endl;
+}
+
 /**
  * @brief Main of the Host Application.
  */
@@ -103,12 +118,23 @@ int main(int argc, char **argv)
   struct Params params = input_params(argc, argv);
 
   std::cout << "Running CPU string match for string size: " << params.stringLength << ", key size: " << params.keyLength << "\n";
-  string haystack, needle;
+  string haystack;
+  vector<string> needles;
   vector<uint8_t> matches;
   if (params.inputFile == nullptr)
   {
-    getString(haystack, params.stringLength);
-    getString(needle, params.keyLength);
+    // getString(haystack, params.stringLength);
+    // for(uint64_t i=0; i < params.numKeys; ++i) {
+    //   needles.push_back("");
+    //   getString(needles.back(), params.keyLength);
+    // }
+
+    // Potential problem scenario:
+    // Original looks for a line to match completely, not just an occurance
+    // In the below example, original would return false for "eld"
+    // However, my current implementation would return true for "eld"
+    haystack = "abc\ndef\nghi\neldkslkdfj\nhelloworld";
+    needles = {"abc", "lmp", "helloworld", "eld"};
   } 
   else 
   {
@@ -116,16 +142,28 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  matches.resize(haystack.size());
+  matches.resize(needles.size());
+
+  // Needles are already encrypted
+
+  for(string& needle : needles) {
+    for(uint64_t i=0; i<needle.size(); ++i) {
+      needle[i] += CHAR_OFFSET;
+    }
+  }
 
   auto start = std::chrono::high_resolution_clock::now();
-  
-  string_match(needle, haystack, matches);
+
+  for (int32_t i = 0; i < WARMUP; i++)
+  {
+    string_match(needles, haystack, matches);
+  }
 
   auto end = std::chrono::high_resolution_clock::now();
 
   std::chrono::duration<double, std::milli> elapsedTime = (end - start)/WARMUP;
   std::cout << "Duration: " << std::fixed << std::setprecision(3) << elapsedTime.count() << " ms." << std::endl;
+  printVec(matches);
 
   return 0;
 }
