ab5ca02d6bf96f3b596242ab38861072dab33abe
fzx333 <fjy3ws>
2024-10-20 16:02:58 -0400

    Code clean up.

--- libpimeval/src/pimCmd.cpp
+++ libpimeval/src/pimCmd.cpp
@@ -387,29 +387,27 @@ pimCmdFunc1::computeRegion(unsigned index)
   unsigned numElementsInRegion = srcRegion.getNumElemInRegion();
   for (unsigned j = 0; j < numElementsInRegion; ++j) {
     uint64_t elemIdx = elemIdxBegin + j;
-    if (dataType == PIM_INT8 || dataType == PIM_INT16 || dataType == PIM_INT32 || dataType == PIM_INT64 || dataType == PIM_UINT8 || dataType == PIM_UINT16 || dataType == PIM_UINT32 || dataType == PIM_UINT64 || dataType == PIM_FP32) {
-      bool isSigned = (dataType == PIM_INT8 || dataType == PIM_INT16 || dataType == PIM_INT32 || dataType == PIM_INT64);
-      bool isUnsigned = (dataType == PIM_UINT8 || dataType == PIM_UINT16 || dataType == PIM_UINT32 || dataType == PIM_UINT64);
-      bool isFP = (dataType == PIM_FP32);
-      if (isSigned) {
-        int64_t signedOperand = objSrc.getElementBits(elemIdx);
-        int64_t result = 0;
-        if(!computeResult(signedOperand, m_cmdType, (int64_t)m_scalarValue, result, bitsPerElementSrc)) return false;
-        objDest.setElement(elemIdx, result);
-      } else if (isUnsigned) {
-        uint64_t unsignedOperand = objSrc.getElementBits(elemIdx);
-        uint64_t result = 0;
-        if(!computeResult(unsignedOperand, m_cmdType, m_scalarValue, result, bitsPerElementSrc)) return false;
-        objDest.setElement(elemIdx, result);
-      } else if (isFP){
-        uint64_t bits = objSrc.getElementBits(elemIdx);
-        float floatOperand = pimUtils::castBitsToType<float>(bits);
-        float result = 0.0;
-        if(!computeResultFP(floatOperand, m_cmdType, pimUtils::castBitsToType<float>(m_scalarValue), result)) return false;
-        objDest.setElement(elemIdx, result);
-      } else {
-      assert(0); // todo: data type
-      }
+    bool isSigned = (dataType == PIM_INT8 || dataType == PIM_INT16 || dataType == PIM_INT32 || dataType == PIM_INT64);
+    bool isUnsigned = (dataType == PIM_UINT8 || dataType == PIM_UINT16 || dataType == PIM_UINT32 || dataType == PIM_UINT64);
+    bool isFP = (dataType == PIM_FP32);
+    if (isSigned) {
+      int64_t signedOperand = objSrc.getElementBits(elemIdx);
+      int64_t result = 0;
+      if(!computeResult(signedOperand, m_cmdType, (int64_t)m_scalarValue, result, bitsPerElementSrc)) return false;
+      objDest.setElement(elemIdx, result);
+    } else if (isUnsigned) {
+      uint64_t unsignedOperand = objSrc.getElementBits(elemIdx);
+      uint64_t result = 0;
+      if(!computeResult(unsignedOperand, m_cmdType, m_scalarValue, result, bitsPerElementSrc)) return false;
+      objDest.setElement(elemIdx, result);
+    } else if (isFP){
+      uint64_t bits = objSrc.getElementBits(elemIdx);
+      float floatOperand = pimUtils::castBitsToType<float>(bits);
+      float result = 0.0;
+      if(!computeResultFP(floatOperand, m_cmdType, pimUtils::castBitsToType<float>(m_scalarValue), result)) return false;
+      objDest.setElement(elemIdx, result);
+    } else {
+    assert(0); // todo: data type
     }
   }
   return true;
@@ -495,35 +493,33 @@ pimCmdFunc2::computeRegion(unsigned index)
   unsigned numElementsInRegion = src1Region.getNumElemInRegion();
   for (unsigned j = 0; j < numElementsInRegion; ++j) {
     uint64_t elemIdx = elemIdxBegin + j;
-    if (dataType == PIM_INT8 || dataType == PIM_INT16 || dataType == PIM_INT32 || dataType == PIM_INT64 || dataType == PIM_UINT8 || dataType == PIM_UINT16 || dataType == PIM_UINT32 || dataType == PIM_UINT64 || dataType == PIM_FP32) {
-      bool isSigned = (dataType == PIM_INT8 || dataType == PIM_INT16 || dataType == PIM_INT32 || dataType == PIM_INT64);
-      bool isUnsigned = (dataType == PIM_UINT8 || dataType == PIM_UINT16 || dataType == PIM_UINT32 || dataType == PIM_UINT64);
-      bool isFP = (dataType == PIM_FP32);
-      if (isSigned) {
-        uint64_t operandBits1 = objSrc1.getElementBits(elemIdx);
-        uint64_t operandBits2 = objSrc2.getElementBits(elemIdx);
-        int64_t operand1 = pimUtils::signExt(operandBits1, dataType);
-        int64_t operand2 = pimUtils::signExt(operandBits2, dataType);
-        int64_t result = 0;
-        if(!computeResult(operand1, operand2, m_cmdType, (int64_t)m_scalarValue, result)) return false;
-        objDest.setElement(elemIdx, result);
-      } else if (isUnsigned) {
-        uint64_t unsignedOperand1 = objSrc1.getElementBits(elemIdx);
-        uint64_t unsignedOperand2 = objSrc2.getElementBits(elemIdx);
-        uint64_t result = 0;
-        if(!computeResult(unsignedOperand1, unsignedOperand2, m_cmdType, m_scalarValue, result)) return false;
-        objDest.setElement(elemIdx, result);
-      } else if (isFP){
-        uint64_t operandBits1 = objSrc1.getElementBits(elemIdx);
-        uint64_t operandBits2 = objSrc2.getElementBits(elemIdx);
-        float floatOperand1 = pimUtils::castBitsToType<float>(operandBits1);
-        float floatOperand2 = pimUtils::castBitsToType<float>(operandBits2);
-        float result = 0.0;
-        if(!computeResultFP(floatOperand1, floatOperand2, m_cmdType, pimUtils::castBitsToType<float>(m_scalarValue), result)) return false;
-        objDest.setElement(elemIdx, result);
-      } else {
-      assert(0); // todo: data type
-      }
+    bool isSigned = (dataType == PIM_INT8 || dataType == PIM_INT16 || dataType == PIM_INT32 || dataType == PIM_INT64);
+    bool isUnsigned = (dataType == PIM_UINT8 || dataType == PIM_UINT16 || dataType == PIM_UINT32 || dataType == PIM_UINT64);
+    bool isFP = (dataType == PIM_FP32);
+    if (isSigned) {
+      uint64_t operandBits1 = objSrc1.getElementBits(elemIdx);
+      uint64_t operandBits2 = objSrc2.getElementBits(elemIdx);
+      int64_t operand1 = pimUtils::signExt(operandBits1, dataType);
+      int64_t operand2 = pimUtils::signExt(operandBits2, dataType);
+      int64_t result = 0;
+      if(!computeResult(operand1, operand2, m_cmdType, (int64_t)m_scalarValue, result)) return false;
+      objDest.setElement(elemIdx, result);
+    } else if (isUnsigned) {
+      uint64_t unsignedOperand1 = objSrc1.getElementBits(elemIdx);
+      uint64_t unsignedOperand2 = objSrc2.getElementBits(elemIdx);
+      uint64_t result = 0;
+      if(!computeResult(unsignedOperand1, unsignedOperand2, m_cmdType, m_scalarValue, result)) return false;
+      objDest.setElement(elemIdx, result);
+    } else if (isFP){
+      uint64_t operandBits1 = objSrc1.getElementBits(elemIdx);
+      uint64_t operandBits2 = objSrc2.getElementBits(elemIdx);
+      float floatOperand1 = pimUtils::castBitsToType<float>(operandBits1);
+      float floatOperand2 = pimUtils::castBitsToType<float>(operandBits2);
+      float result = 0.0;
+      if(!computeResultFP(floatOperand1, floatOperand2, m_cmdType, pimUtils::castBitsToType<float>(m_scalarValue), result)) return false;
+      objDest.setElement(elemIdx, result);
+    } else {
+    assert(0); // todo: data type
     }
   }
   return true;
