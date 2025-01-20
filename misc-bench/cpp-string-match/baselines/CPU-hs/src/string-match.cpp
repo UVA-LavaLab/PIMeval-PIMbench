982bc4659921ed01256f4903a481601e514bbf45
Ethan Ermovick <keg9ve@virginia.edu>
2025-01-20 18:31:29 -0500

    update cpu string match to give output in pfac's form

--- misc-bench/cpp-string-match/baselines/CPU-hs/src/string-match.cpp
+++ misc-bench/cpp-string-match/baselines/CPU-hs/src/string-match.cpp
@@ -16,16 +16,15 @@
 #include <iostream>
 #include <vector>
 #include <hs.h>
+#include <fstream>
 
 #include "utilBaselines.h"
 
 // Params ---------------------------------------------------------------------
 typedef struct Params
 {
-  uint64_t stringLength;
-  uint64_t keyLength;
-  uint64_t numKeys;
-  char *inputFile;
+  char *keysInputFile;
+  char *textInputFile;
 } Params;
 
 void usage()
@@ -33,23 +32,19 @@ void usage()
   fprintf(stderr,
           "\nUsage:  ./string-match.out [options]"
           "\n"
-          "\n    -s    string size (default=2048 elements)"
-          "\n    -k    key size (default = 20 elements)"
-          "\n    -n    number of keys (default = 4 keys)"
-          "\n    -i    input file containing string and keys (default=generates strings with random characters)"
+          "\n    -k    keys input file, with each key on a seperate line (required, searches in cpp-string-match/dataset directory, note that keys are expected to be sorted by length, with smaller keys first)"
+          "\n    -t    text input file to search for keys from (required, searches in cpp-string-match/dataset directory)"
           "\n");
 }
 
 struct Params input_params(int argc, char **argv)
 {
   struct Params p;
-  p.stringLength = 2048;
-  p.keyLength = 20;
-  p.numKeys = 4;
-  p.inputFile = nullptr;
+  p.keysInputFile = nullptr;
+  p.textInputFile = nullptr;
 
   int opt;
-  while ((opt = getopt(argc, argv, "h:s:k:n:i:")) >= 0)
+  while ((opt = getopt(argc, argv, "h:k:t:")) >= 0)
   {
     switch (opt)
     {
@@ -57,17 +52,11 @@ struct Params input_params(int argc, char **argv)
       usage();
       exit(0);
       break;
-    case 's':
-      p.stringLength = strtoull(optarg, NULL, 0);
-      break;
     case 'k':
-      p.keyLength = strtoull(optarg, NULL, 0);
+      p.keysInputFile = optarg;
       break;
-    case 'n':
-      p.numKeys = strtoull(optarg, NULL, 0);
-      break;
-    case 'i':
-      p.inputFile = optarg;
+    case 't':
+      p.textInputFile = optarg;
       break;
     default:
       fprintf(stderr, "\nUnrecognized option!\n");
@@ -78,81 +67,24 @@ struct Params input_params(int argc, char **argv)
   return p;
 }
 
-// hs_database_t* compile_pattern(const char* haystack) {
-//     hs_database_t *database;
-//     hs_compile_error_t *compile_err;
-//     if (hs_compile(haystack, HS_FLAG_DOTALL, HS_MODE_BLOCK, NULL, &database,
-//                    &compile_err) != HS_SUCCESS) {
-//         fprintf(stderr, "Hyperscan couldn't compile pattern, exiting\"%s\": %s\n",
-//                 haystack, compile_err->message);
-//         hs_free_compile_error(compile_err);
-//         return nullptr;
-//     }
-// }
-
 static int match_callback(unsigned int id, unsigned long long from,
                         unsigned long long to, unsigned int flags, void *matches) {
-    printf("Match for pattern from: %llu, id: %u, to: %llu \n", from, id, to);
+    // printf("Match for pattern from: %llu, id: %u, to: %llu \n", from, id, to);
     // ((uint8_t*) ctx)[from] = 1;
-    (*(vector<vector<uint8_t>>*)matches)[id][from] = 1;
-    printf("set matches arr\n");
+    (*(std::vector<int>*)matches)[from] = max(
+      (*(std::vector<int>*)matches)[from]
+      , (int) id);
+    // printf("set matches arr\n");
     return 0;
 }
 
-/**
- * @brief cpu string match kernel
- */
-// int string_match_cpu(vector<string>& needles, string& haystack, vector<vector<uint8_t>>& matches, hs_database_t* db, hs_scratch_t* scratch) {
 
-//   if(hs_scan(db, haystack.c_str(), haystack.size(), 0, scratch, match_callback, (void*) &matches) != HS_SUCCESS) {
-//     fprintf(stderr, "Hyperscan couldn't scan the haystack, exiting\n");
-//     hs_free_scratch(scratch);
-//     hs_free_database(db);
-//     return -1;
-//   }
-
-//   return 0;
-
-//   // for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
-//   //   hs_database_t *database;
-//   //   hs_compile_error_t *compile_err;
-
-//   //   // TODO: Replace with hs_compile_lit_multi
-//   //   if(hs_compile_lit(needles[needle_idx].c_str(), HS_FLAG_SOM_LEFTMOST, needles[needle_idx].size(), HS_MODE_BLOCK, NULL, &database, &compile_err) != HS_SUCCESS) {
-//   //       fprintf(stderr, "Hyperscan couldn't compile pattern, exiting\"%s\": %s\n", haystack.c_str(), compile_err->message);
-//   //       hs_free_compile_error(compile_err);
-//   //       return;
-//   //   }
-
-//   //   hs_scratch_t *scratch = NULL;
-//   //   if (hs_alloc_scratch(database, &scratch) != HS_SUCCESS) {
-//   //       fprintf(stderr, "Hyperscan couldn't allocate scratch memory, exiting\n");
-//   //       hs_free_database(database);
-//   //       return;
-//   //   }
-//   //   // matches[needle_idx].data()
-//   //   if(hs_scan(database, haystack.c_str(), haystack.size(), 0, scratch, match_callback, (void*) matches[needle_idx].data()) != HS_SUCCESS) {
-//   //     fprintf(stderr, "Hyperscan couldn't scan the haystack, exiting\n");
-//   //     hs_free_scratch(scratch);
-//   //     hs_free_database(database);
-//   //     return;
-//   //   }
-//   // }
-// }
-
-void getString(string& str, uint64_t len) {
-  str.resize(len);
-#pragma omp parallel for
-  for(uint64_t i=0; i<len; ++i) {
-    str[i] = 'a' + (rand()%26);
+template <typename T>
+void printVec(std::vector<T>& vec) {
+  for(T elem : vec) {
+    cout << elem << ", ";
   }
-}
-
-void printVec(vector<uint8_t>& vec) {
-  for(auto match : vec) {
-    cout << (unsigned) (match) << ", ";
-  }
-  cout << endl;
+  std::cout << std::endl;
 }
 
 /**
@@ -162,32 +94,49 @@ int main(int argc, char **argv)
 {
   struct Params params = input_params(argc, argv);
 
-  std::cout << "Running CPU string match for string size: " << params.stringLength << ", key size: " << params.keyLength << ", number of keys: " << params.numKeys << "\n";
-  string haystack;
-  vector<string> needles;
-  vector<vector<uint8_t>> matches;
-  if (params.inputFile == nullptr)
-  {
-    getString(haystack, params.stringLength);
-    for(uint64_t i=0; i < params.numKeys; ++i) {
-      needles.push_back("");
-      getString(needles.back(), params.keyLength);
-    }
-
-    haystack = "dabcslkdfjdljed";
-    needles = {"abc", "d", "z", "bcslk", "dabcs"};
-  } 
-  else 
-  {
-    std::cout << "Reading from input file is not implemented yet." << std::endl;
+  if(params.keysInputFile == nullptr) {
+    std::cout << "Please provide a keys input file" << std::endl;
+    return 1;
+  }
+  if(params.textInputFile == nullptr) {
+    std::cout << "Please provide a text input file" << std::endl;
     return 1;
   }
 
-  matches.resize(needles.size());
+  std::cout << "Running CPU string match for \"" << params.keysInputFile << "\" as the keys file, and \"" << params.textInputFile << "\" as the text input file\n";
+  std::string haystack;
+  std::vector<std::string> needles;
+  std::vector<int> matches;
 
-  for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
-    matches[needle_idx].resize(haystack.size());
+  const std::string DATASET_FOLDER_PREFIX = "./../../../dataset/";
+
+  std::string text_input_file = params.textInputFile;
+  text_input_file = DATASET_FOLDER_PREFIX + text_input_file;
+  
+  std::ifstream text_file(text_input_file);
+  if (!text_file) {
+    std::cout << "Couldn't open text input file" << std::endl;
+    return 1;
   }
+
+  std::ostringstream text_file_oss;
+  text_file_oss << text_file.rdbuf();
+  haystack = text_file_oss.str();
+
+  std::string keys_input_file = params.keysInputFile;
+  keys_input_file = DATASET_FOLDER_PREFIX + keys_input_file;
+  std::ifstream keys_file(keys_input_file);
+  if (!keys_file) {
+    std::cout << "Couldn't open keys input file" << std::endl;
+    return 1;
+  }
+  std::string line;
+
+  while (std::getline(keys_file, line)) {
+      needles.push_back(line);
+  }
+
+  matches.resize(haystack.size());
 
   char** needles_arr = (char**) malloc(sizeof(char*) * needles.size());
   unsigned* flags_arr = (unsigned*) malloc(sizeof(unsigned) * needles.size());
@@ -202,7 +151,7 @@ int main(int argc, char **argv)
   for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
     needles_arr[needle_idx] = (char*) needles[needle_idx].c_str();
     flags_arr[needle_idx] = HS_FLAG_SOM_LEFTMOST;
-    ids_arr[needle_idx] = needle_idx;
+    ids_arr[needle_idx] = 1 + needle_idx;
     lens_arr[needle_idx] = needles[needle_idx].size();
   }
 
@@ -242,9 +191,8 @@ int main(int argc, char **argv)
 
   std::chrono::duration<double, std::milli> elapsedTime = (end - start)/WARMUP;
   std::cout << "Duration: " << std::fixed << std::setprecision(3) << elapsedTime.count() << " ms." << std::endl;
-  for(vector<uint8_t> match : matches) {
-    printVec(match);
-  }
+  
+  printVec(matches);
 
   hs_free_database(db);
   hs_free_scratch(scratch);
