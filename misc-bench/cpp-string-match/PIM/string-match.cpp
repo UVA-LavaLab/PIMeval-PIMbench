7d33118b6c291b18f6cfbad1ab8932169857071e
Ethan Ermovick <keg9ve@virginia.edu>
2025-01-20 19:07:42 -0500

    start updating PIM string match

--- misc-bench/cpp-string-match/PIM/string-match.cpp
+++ misc-bench/cpp-string-match/PIM/string-match.cpp
@@ -16,16 +16,12 @@
 #include "../../util.h"
 #include "libpimeval.h"
 
-using namespace std;
-
 // Params ---------------------------------------------------------------------
 typedef struct Params
 {
-  uint64_t stringLength;
-  uint64_t keyLength;
-  uint64_t numKeys;
+  char *keysInputFile;
+  char *textInputFile;
   char *configFile;
-  char *inputFile;
   bool shouldVerify;
 } Params;
 
@@ -34,11 +30,9 @@ void usage()
   fprintf(stderr,
           "\nUsage:  ./string-match.out [options]"
           "\n"
-          "\n    -s    string size (default=2048 elements)"
-          "\n    -k    key size (default = 20 elements)"
-          "\n    -n    number of keys (default = 4 keys)"
+          "\n    -k    keys input file, with each key on a seperate line (required, searches in cpp-string-match/dataset directory, note that keys are expected to be sorted by length, with smaller keys first)"
+          "\n    -t    text input file to search for keys from (required, searches in cpp-string-match/dataset directory)"
           "\n    -c    dramsim config file"
-          "\n    -i    input file containing string and keys (default=generates strings with random characters)"
           "\n    -v    t = verifies PIM output with host output. (default=false)"
           "\n");
 }
@@ -46,15 +40,13 @@ void usage()
 struct Params getInputParams(int argc, char **argv)
 {
   struct Params p;
-  p.stringLength = 2048;
-  p.keyLength = 20;
-  p.numKeys = 4;
+  p.keysInputFile = nullptr;
+  p.textInputFile = nullptr;
   p.configFile = nullptr;
-  p.inputFile = nullptr;
   p.shouldVerify = false;
 
   int opt;
-  while ((opt = getopt(argc, argv, "h:s:k:n:c:i:v:")) >= 0)
+  while ((opt = getopt(argc, argv, "h:k:t:c:v:")) >= 0)
   {
     switch (opt)
     {
@@ -62,20 +54,14 @@ struct Params getInputParams(int argc, char **argv)
       usage();
       exit(0);
       break;
-    case 's':
-      p.stringLength = strtoull(optarg, NULL, 0);
-      break;
     case 'k':
-      p.keyLength = strtoull(optarg, NULL, 0);
+      p.keysInputFile = optarg;
       break;
-    case 'n':
-      p.numKeys = strtoull(optarg, NULL, 0);
+    case 't':
+      p.textInputFile = optarg;
       break;
     case 'c':
       p.configFile = optarg;
-      break;
-    case 'i':
-      p.inputFile = optarg;
       break;
     case 'v':
       p.shouldVerify = (*optarg == 't') ? true : false;
@@ -115,18 +101,17 @@ void print_pim_int(PimObjId pim_obj, uint64_t len) {
   std::cout << std::endl;
 }
 
-void string_match(vector<string>& needles, string& haystack, vector<vector<uint8_t>>& matches) {
+void string_match(std::vector<std::string>& needles, std::string& haystack, std::vector<int>& matches) {
 
   uint64_t num_needles = needles.size();
 
   PimObjId haystack_pim = pimAlloc(PIM_ALLOC_AUTO, haystack.size(), PIM_UINT8);
   assert(haystack_pim != -1);
-
-  // intermediate_pim, and pim_results[i] can be replaced with booleans/1 bit ints
+  
   PimObjId intermediate_pim = pimAllocAssociated(haystack_pim, PIM_UINT8);
   assert(intermediate_pim != -1);
 
-  vector<PimObjId> pim_results;
+  std::vector<PimObjId> pim_results;
   for(uint64_t i=0; i<num_needles; ++i) {
     pim_results.push_back(pimAllocAssociated(haystack_pim, PIM_UINT8));
     assert(pim_results.back() != -1);
@@ -172,48 +157,71 @@ void string_match(vector<string>& needles, string& haystack, vector<vector<uint8
   }
 }
 
-void string_match_cpu(vector<string>& needles, string& haystack, vector<vector<uint8_t>>& matches) {
+// void string_match_cpu(vector<string>& needles, string& haystack, vector<vector<uint8_t>>& matches) {
 
-  for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
-    size_t pos = haystack.find(needles[needle_idx], 0);
+//   for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
+//     size_t pos = haystack.find(needles[needle_idx], 0);
 
-    while (pos != string::npos) {
-        matches[needle_idx][pos] = 1;
-        pos = haystack.find(needles[needle_idx], pos + 1);
-    }
+//     while (pos != string::npos) {
+//         matches[needle_idx][pos] = 1;
+//         pos = haystack.find(needles[needle_idx], pos + 1);
+//     }
+//   }
+// }
+
+template <typename T>
+void printVec(std::vector<T>& vec) {
+  for(T elem : vec) {
+    cout << elem << ", ";
   }
-}
-
-void getString(string& str, uint64_t len) {
-  str.resize(len);
-#pragma omp parallel for
-  for(uint64_t i=0; i<len; ++i) {
-    str[i] = 'a' + (rand()%26);
-  }
+  std::cout << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
   struct Params params = getInputParams(argc, argv);
-  std::cout << "Running PIM string match for string size: " << params.stringLength << ", key size: " << params.keyLength << ", number of keys: " << params.numKeys << "\n";
-  string haystack;
-  vector<string> needles;
-  vector<vector<uint8_t>> matches;
-  if (params.inputFile == nullptr)
-  {
-    getString(haystack, params.stringLength);
-    for(uint64_t i=0; i < params.numKeys; ++i) {
-      needles.push_back("");
-      getString(needles.back(), params.keyLength);
-    }
-    
-    // haystack = "dabcslkdfjdljed";
-    // needles = {"abc", "d", "z", "bcslk", "dabcs"};
-  } 
-  else 
-  {
-    std::cout << "Reading from input file is not implemented yet." << std::endl;
+  
+  if(params.keysInputFile == nullptr) {
+    std::cout << "Please provide a keys input file" << std::endl;
     return 1;
+  }
+  if(params.textInputFile == nullptr) {
+    std::cout << "Please provide a text input file" << std::endl;
+    return 1;
+  }
+  
+  std::cout << "Running PIM string match for \"" << params.keysInputFile << "\" as the keys file, and \"" << params.textInputFile << "\" as the text input file\n";
+  
+  std::string haystack;
+  std::vector<std::string> needles;
+  std::vector<int> matches;
+
+  const std::string DATASET_FOLDER_PREFIX = "./../dataset/";
+
+  std::string text_input_file = params.textInputFile;
+  text_input_file = DATASET_FOLDER_PREFIX + text_input_file;
+  
+  std::ifstream text_file(text_input_file);
+  if (!text_file) {
+    std::cout << "Couldn't open text input file" << std::endl;
+    return 1;
+  }
+
+  std::ostringstream text_file_oss;
+  text_file_oss << text_file.rdbuf();
+  haystack = text_file_oss.str();
+
+  std::string keys_input_file = params.keysInputFile;
+  keys_input_file = DATASET_FOLDER_PREFIX + keys_input_file;
+  std::ifstream keys_file(keys_input_file);
+  if (!keys_file) {
+    std::cout << "Couldn't open keys input file" << std::endl;
+    return 1;
+  }
+  std::string line;
+
+  while (std::getline(keys_file, line)) {
+      needles.push_back(line);
   }
   
   if (!createDevice(params.configFile))
@@ -221,45 +229,44 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  matches.resize(needles.size());
+  matches.resize(haystack.size(), 0);
 
-  for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
-    matches[needle_idx].resize(haystack.size());
-  }
-
-  //TODO: Check if vector can fit in one iteration. Otherwise need to run in multiple iteration.
+  // //TODO: Check if vector can fit in one iteration. Otherwise need to run in multiple iteration.
   string_match(needles, haystack, matches);
 
-  if (params.shouldVerify) 
-  {
-    vector<vector<uint8_t>> matches_cpu;
+  std::cout << "matches: ";
+  printVec(matches);
+
+  // if (params.shouldVerify) 
+  // {
+  //   vector<vector<uint8_t>> matches_cpu;
     
-    matches_cpu.resize(needles.size());
+  //   matches_cpu.resize(needles.size());
 
-    for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
-      matches_cpu[needle_idx].resize(haystack.size());
-    }
+  //   for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
+  //     matches_cpu[needle_idx].resize(haystack.size());
+  //   }
 
-    string_match_cpu(needles, haystack, matches_cpu);
+  //   string_match_cpu(needles, haystack, matches_cpu);
 
-    // verify result
-    bool is_correct = true;
-    #pragma omp parallel for
-    for (unsigned i = 0; i < matches.size(); ++i)
-    {
-      for (unsigned j = 0; j < haystack.size(); ++j)
-      {
-        if (matches[i][j] != matches_cpu[i][j])
-        {
-          std::cout << "Wrong answer: " << unsigned(matches[i][j]) << " (expected " << unsigned(matches_cpu[i][j]) << "), for needle: " << i << " at position " << j << std::endl;
-          is_correct = false;
-        }
-      }
-    }
-    if(is_correct) {
-      std::cout << "Correct for string match!" << std::endl;
-    }
-  }
+  //   // verify result
+  //   bool is_correct = true;
+  //   #pragma omp parallel for
+  //   for (unsigned i = 0; i < matches.size(); ++i)
+  //   {
+  //     for (unsigned j = 0; j < haystack.size(); ++j)
+  //     {
+  //       if (matches[i][j] != matches_cpu[i][j])
+  //       {
+  //         std::cout << "Wrong answer: " << unsigned(matches[i][j]) << " (expected " << unsigned(matches_cpu[i][j]) << "), for needle: " << i << " at position " << j << std::endl;
+  //         is_correct = false;
+  //       }
+  //     }
+  //   }
+  //   if(is_correct) {
+  //     std::cout << "Correct for string match!" << std::endl;
+  //   }
+  // }
 
   pimShowStats();
 
