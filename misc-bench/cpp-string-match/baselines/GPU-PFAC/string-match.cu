61099b85cc4b150267582b1ffd903e341085a3a4
Ethan Ermovick <keg9ve@virginia.edu>
2025-01-21 22:55:01 -0500

    string match: Implement PFAC

--- misc-bench/cpp-string-match/baselines/GPU-PFAC/string-match.cu
+++ misc-bench/cpp-string-match/baselines/GPU-PFAC/string-match.cu
@@ -1,36 +1,27 @@
-/**
- * @file string-match.cu
- * @brief Template for a Host Application Source File.
- *
- */
+// Test: Cuda version of string match
+// Copyright (c) 2025 University of Virginia
+// This file is licensed under the MIT License.
+// See the LICENSE file in the root of this repository for more details.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <assert.h>
-#include <stdint.h>
-#include <iomanip>
-#include <chrono>
-#include <omp.h>
 #include <iostream>
 #include <vector>
-#include <memory>
+#include <getopt.h>
+#include <stdint.h>
+#include <iomanip>
+#include <cassert>
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
 
-#include "utilBaselines.h"
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include "string-match-utils.h"
+#include "PFAC.h"
 
 // Params ---------------------------------------------------------------------
 typedef struct Params
 {
-  uint64_t stringLength;
-  uint64_t keyLength;
-  uint64_t numKeys;
   char *keysInputFile;
   char *textInputFile;
+  bool shouldVerify;
 } Params;
 
 void usage()
@@ -38,25 +29,21 @@ void usage()
   fprintf(stderr,
           "\nUsage:  ./string-match.out [options]"
           "\n"
-          "\n    -s    string size (default=2048 elements)"
-          "\n    -k    key size (default = 20 elements)"
-          "\n    -n    number of keys (default = 4 keys)"
-          "\n    -i    input file containing keys to search for (default=generates keys with random characters)"
-          "\n    -t    input file containing string to search in (default=generates random string)"
+          "\n    -k    keys input file, with each key on a seperate line (required, searches in cpp-string-match/dataset directory, note that keys are expected to be sorted by length, with smaller keys first)"
+          "\n    -t    text input file to search for keys from (required, searches in cpp-string-match/dataset directory)"
+          "\n    -v    t = verifies PIM output with host output. (default=false)"
           "\n");
 }
 
-struct Params input_params(int argc, char **argv)
+struct Params getInputParams(int argc, char **argv)
 {
   struct Params p;
-  p.stringLength = 2048;
-  p.keyLength = 20;
-  p.numKeys = 4;
   p.keysInputFile = nullptr;
   p.textInputFile = nullptr;
+  p.shouldVerify = false;
 
   int opt;
-  while ((opt = getopt(argc, argv, "h:s:k:n:i:t:")) >= 0)
+  while ((opt = getopt(argc, argv, "h:k:t:v:")) >= 0)
   {
     switch (opt)
     {
@@ -64,20 +51,14 @@ struct Params input_params(int argc, char **argv)
       usage();
       exit(0);
       break;
-    case 's':
-      p.stringLength = strtoull(optarg, NULL, 0);
-      break;
     case 'k':
-      p.keyLength = strtoull(optarg, NULL, 0);
-      break;
-    case 'n':
-      p.numKeys = strtoull(optarg, NULL, 0);
-      break;
-    case 'i':
       p.keysInputFile = optarg;
       break;
     case 't':
       p.textInputFile = optarg;
+      break;
+    case 'v':
+      p.shouldVerify = (*optarg == 't') ? true : false;
       break;
     default:
       fprintf(stderr, "\nUnrecognized option!\n");
@@ -88,114 +69,176 @@ struct Params input_params(int argc, char **argv)
   return p;
 }
 
-void getString(string& str, uint64_t len) {
-  str.resize(len);
-#pragma omp parallel for
-  for(uint64_t i=0; i<len; ++i) {
-    str[i] = 'a' + (rand()%26);
+template <typename T>
+void printVec(std::vector<T>& vec) {
+  for(T elem : vec) {
+    std::cout << elem << ", ";
   }
+  std::cout << std::endl;
 }
 
-void printVec(vector<uint8_t>& vec) {
-  for(auto match : vec) {
-    cout << (unsigned) (match) << ", ";
-  }
-  cout << endl;
-}
-
-/**
- * @brief Main of the Host Application.
- */
-int main(int argc, char **argv)
-{
-  struct Params params = input_params(argc, argv);
-
-  std::cout << "Running CPU string match for string size: " << params.stringLength << ", key size: " << params.keyLength << ", number of keys: " << params.numKeys << "\n";
-  string haystack;
-  vector<string> needles;
-  vector<vector<uint8_t>> matches;
-
-  if (params.keysInputFile == nullptr)
-  {
-    for(uint64_t i=0; i < params.numKeys; ++i) {
-      needles.push_back("");
-      getString(needles.back(), params.keyLength);
-    }
-  } 
-  else 
-  {
-    int keys_fd = open(params.keysInputFile, O_RDONLY);
-    if (keys_fd < 0) 
-    {
-      perror("Failed to open keys input file, or file doesn't exist");
-      return 1;
-    }
-
-    struct stat keys_finfo;
-    if (fstat(keys_fd, &keys_finfo) < 0) 
-    {
-      perror("Failed to get file info for keys");
-      return 1;
-    }
-
-    // char* keys_fdata = (char*) malloc(sizeof(char) * (keys_finfo.st_size + 1));
-    string keys_fdata(keys_finfo.st_size, '\0');
-    // if(keys_fdata == NULL) {
-    //     perror("Failed to allocate memory for keys");
-    //     return 1;
-    // }
-
-    // keys_fdata[keys_finfo.st_size] = '\0';
-    if (read(keys_fd, keys_fdata.data(), keys_finfo.st_size) < 0) 
-    {
-      perror("Failed to read the keys file");
-      return 1;
-    }
-    std::istringstream keys_fdata_stream(keys_fdata);
-    std::string key_line;
-
-    while (getline(keys_fdata_stream, key_line)) {
-        needles.push_back(key_line);
-    }
-
-    cout << keys_fdata << endl;
-  }
-
-  if (params.textInputFile == nullptr)
-  {
-    getString(haystack, params.stringLength);
-  } 
-  else 
-  {
-    int haystack_fd = open(params.textInputFile, O_RDONLY);
-    if (haystack_fd < 0) 
-    {
-      perror("Failed to open text input file, or file doesn't exist");
-      return 1;
-    }
-
-    struct stat haystack_finfo;
-    if (fstat(haystack_fd, &haystack_finfo) < 0) 
-    {
-      perror("Failed to get file info for text");
-      return 1;
-    }
-
-    string haystack_fdata(haystack_finfo.st_size, '\0');
-
-    haystack_fdata[haystack_finfo.st_size] = '\0';
-    if (read(haystack_fd, haystack_fdata.data(), haystack_finfo.st_size) < 0) 
-    {
-      perror("Failed to read the text file");
-      return 1;
-    }
-    cout << haystack_fdata << endl;
-  }
-
-  matches.resize(needles.size());
-
+void string_match_cpu(std::vector<std::string>& needles, std::string& haystack, std::vector<int>& matches) {
   for(uint64_t needle_idx = 0; needle_idx < needles.size(); ++needle_idx) {
-    matches[needle_idx].resize(haystack.size());
+    size_t pos = haystack.find(needles[needle_idx], 0);
+
+    while (pos != std::string::npos) {
+        matches[pos] = std::max((unsigned long) matches[pos], needle_idx + 1);
+        pos = haystack.find(needles[needle_idx], pos + 1);
+    }
+  }
+}
+
+float string_match_gpu(std::string& needle_filename, std::string& haystack, std::vector<int>& matches) {
+  PFAC_handle_t pfac_handle;
+  PFAC_status_t pfac_error;
+  cudaError_t cuda_error;
+
+  pfac_error = PFAC_create(&pfac_handle);
+  if ( PFAC_STATUS_SUCCESS != pfac_error ){
+      std::cerr << "Pfac Error: " << PFAC_getErrorString(pfac_error) << std::endl;
+      exit(1);
+  }
+
+  pfac_error = PFAC_setPlatform(pfac_handle, PFAC_PLATFORM_GPU);
+  if ( PFAC_STATUS_SUCCESS != pfac_error ){
+      std::cerr << "Pfac Error: " << PFAC_getErrorString(pfac_error) << std::endl;
+      exit(1);
+  }
+
+  pfac_error = PFAC_readPatternFromFile(pfac_handle, needle_filename.data());
+  if (PFAC_STATUS_SUCCESS != pfac_error){
+    std::cerr << "Cuda Error: " << PFAC_getErrorString(pfac_error) << std::endl;
+    exit(1);
+  }
+
+  pfac_error = PFAC_setTextureMode(pfac_handle, PFAC_TEXTURE_ON);
+  if ( PFAC_STATUS_SUCCESS != pfac_error ){
+      std::cerr << "Pfac Error: " << PFAC_getErrorString(pfac_error) << std::endl;
+      exit(1);
+  }
+
+  char *gpu_text;
+  int *gpu_matches;
+
+  size_t cuda_to_alloc = (haystack.size() + sizeof(int)-1)/sizeof(int);
+  cuda_error = cudaMalloc((void **) &gpu_text, cuda_to_alloc*sizeof(int));
+  if(cuda_error != cudaSuccess) {
+    std::cerr << "Cuda Error: " << cudaGetErrorString(cuda_error) << std::endl;
+    exit(1);
+  }
+  
+  cuda_error = cudaMalloc((void **) &gpu_matches, haystack.size()*sizeof(int));
+  if(cuda_error != cudaSuccess) {
+    std::cerr << "Cuda Error: " << cudaGetErrorString(cuda_error) << std::endl;
+    exit(1);
+  }
+  
+  cuda_error = cudaMemcpy(gpu_text, haystack.c_str(), haystack.size(), cudaMemcpyHostToDevice);
+  if(cuda_error != cudaSuccess) {
+    std::cerr << "Cuda Error: " << cudaGetErrorString(cuda_error) << std::endl;
+    exit(1);
+  }
+
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  float timeElapsed = 0;
+
+  cudaEventRecord(start, 0);
+
+  pfac_error = PFAC_matchFromDevice( pfac_handle, gpu_text, haystack.size()*sizeof(int), gpu_matches);
+  if ( PFAC_STATUS_SUCCESS != pfac_error ){
+      std::cerr << "Pfac Error: " << PFAC_getErrorString(pfac_error) << std::endl;
+      exit(1);
+  }
+  
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&timeElapsed, start, stop);
+
+  cuda_error = cudaGetLastError();
+  if (cuda_error != cudaSuccess) {
+    std::cerr << "Cuda Error: " << cudaGetErrorString(cuda_error) << std::endl;
+    exit(1);
+  }
+
+  pfac_error = PFAC_destroy(pfac_handle);
+  if ( PFAC_STATUS_SUCCESS != pfac_error ){
+      std::cerr << "Pfac Error: " << PFAC_getErrorString(pfac_error) << std::endl;
+      exit(1);
+  }
+
+  cuda_error = cudaMemcpy(matches.data(), gpu_matches, haystack.size() * sizeof(int), cudaMemcpyDeviceToHost);
+  if(cuda_error != cudaSuccess) {
+    std::cerr << "Cuda Error: " << cudaGetErrorString(cuda_error) << std::endl;
+    exit(1);
+  }
+
+  return timeElapsed;
+}
+
+int main(int argc, char* argv[])
+{
+  struct Params params = getInputParams(argc, argv);
+  
+  if(params.keysInputFile == nullptr) {
+    std::cout << "Please provide a keys input file" << std::endl;
+    return 1;
+  }
+  if(params.textInputFile == nullptr) {
+    std::cout << "Please provide a text input file" << std::endl;
+    return 1;
+  }
+  
+  std::cout << "Running GPU string match for \"" << params.keysInputFile << "\" as the keys file, and \"" << params.textInputFile << "\" as the text input file\n";
+  
+  std::string haystack;
+  std::vector<std::string> needles;
+  std::vector<int> matches;
+
+  const std::string DATASET_FOLDER_PREFIX = "./../../dataset/";
+
+  haystack = get_text_from_file(DATASET_FOLDER_PREFIX, params.textInputFile);
+  if(haystack.size() == 0) {
+    std::cout << "There was an error opening the text file" << std::endl;
+    return 1;
+  }
+
+  // needles = get_needles_from_file(DATASET_FOLDER_PREFIX, params.keysInputFile);
+  // if(needles.size() == 0) {
+  //   std::cout << "There was an error opening the keys file" << std::endl;
+  //   return 1;
+  // }
+  matches.resize(haystack.size());
+  std::string keys_filename = DATASET_FOLDER_PREFIX + params.keysInputFile;
+  float timeElapsed = string_match_gpu(keys_filename, haystack, matches);
+  printf("Execution time of string match = %f ms\n", timeElapsed);
+
+  std::cout << "matches: ";
+  printVec(matches);
+
+  if (params.shouldVerify) 
+  {
+    std::vector<int> matches_cpu;
+    
+    matches_cpu.resize(haystack.size(), 0);
+
+    string_match_cpu(needles, haystack, matches_cpu);
+
+    // verify result
+    bool is_correct = true;
+    #pragma omp parallel for
+    for (unsigned i = 0; i < matches.size(); ++i)
+    {
+      if (matches[i] != matches_cpu[i])
+      {
+        std::cout << "Wrong answer: " << unsigned(matches[i]) << " (expected " << unsigned(matches_cpu[i]) << "), for position " << i << std::endl;
+        is_correct = false;
+      }
+    }
+    if(is_correct) {
+      std::cout << "Correct for string match!" << std::endl;
+    }
   }
 
   return 0;
