50796ded224078cd26ae8c482ebf1d05db80be38
Farzana Ahmed Siddique <fas9nw@virginia.edu>
2025-02-14 17:17:16 -0500

    Updated comment libpimeval/src/pimParamsHBMDram.cpp
    Co-authored-by: Alif Ahmed <alifahmed@virginia.edu>

--- libpimeval/src/pimParamsHBMDram.cpp
+++ libpimeval/src/pimParamsHBMDram.cpp
@@ -1,5 +1,5 @@
-// File: pimParamsDDRDram.cc
-// PIMeval Simulator - DDR DRAM parameters
+// File: pimParamsHBMDram.cpp
+// PIMeval Simulator - HBM DRAM parameters
 // Copyright (c) 2024 University of Virginia
 // This file is licensed under the MIT License.
 // See the LICENSE file in the root of this repository for more details.
