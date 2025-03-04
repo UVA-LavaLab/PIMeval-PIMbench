48fba2e015871a8fcf28a553882f4c15331ce407
Ethan Ermovick <keg9ve@virginia.edu>
2025-03-03 20:42:49 -0500

    move data generator functionality to header

--- misc-bench/hamming-string-match/hamming-data-generator/hamming-data-generator.cpp
+++ misc-bench/hamming-string-match/hamming-data-generator/hamming-data-generator.cpp
@@ -22,6 +22,8 @@
 #include <omp.h>
 #endif
 
+#include "utilStringMatch.h"
+
 typedef struct Params
 {
   char *outputFile;
@@ -98,207 +100,16 @@ struct Params getInputParams(int argc, char **argv)
   return p;
 }
 
-//! @brief  Replaces part of a text with random characters
-//! @param[in,out]  text  The text to replace part of
-//! @param[in]  idx  The start index to replace from
-//! @param[in]  key  The key to insert into the text
-//! @param[in]  maxHammingDistance  The maximum number of character changes to the key before insertion
-//! @param[in]  gen  Random number generator
-void hammingTextReplace(std::string& text, const uint64_t idx, const std::string& key, const uint64_t maxHammingDistance, std::mt19937& gen) {
-  std::uniform_int_distribution<> numCharsDist(0, std::min(key.size(), maxHammingDistance));
-  uint64_t numCharsToSwitch = numCharsDist(gen);
-  std::uniform_int_distribution<> charsDist('a', 'b');
-  for(uint64_t keyIdx=0; keyIdx < key.size(); ++keyIdx) {
-    char nextChar;
-    if(numCharsToSwitch) {
-      nextChar = charsDist(gen);
-      --numCharsToSwitch;
-    } else {
-      nextChar = key[keyIdx];
-    }
-    text[idx + keyIdx] = nextChar;
-  }
-}
-
 int main(int argc, char* argv[])
 {
 
   struct Params params = getInputParams(argc, argv);
   std::cout << "Generating Data..." << std::endl;
 
-  if(params.outputFile == nullptr) {
-    std::cerr << "Output filename must be provided!" << std::endl;
-    return 1;
-  }
-
-  double maxPossibleKeys = 0;
-  for (size_t length = params.minKeyLen; length <= params.maxKeyLen; ++length) {
-    maxPossibleKeys += std::pow(26, length);
-  }
-
-  if((double) params.numKeys > maxPossibleKeys) {
-    std::cerr << "Error: Number of keys greater than max possible keys for the given length range" << std::endl;
-    std::cerr << "Requested Keys: " << params.numKeys << ", max number of unique keys of length [" << params.minKeyLen << ", " << params.maxKeyLen << "]: " << std::fixed << std::setprecision(0) << maxPossibleKeys << std::endl;
-    return 1;
-  }
-
-  std::unordered_set<std::string> keysSet;
-  keysSet.reserve(params.numKeys);
-
-  // Create params.numKeys randomly generated keys of random sizes in range [params.minKeyLen, params.maxKeyLen]
-  // All keys are unique
-  while(keysSet.size() < params.numKeys) {
-    size_t currKeyLen = rand()%(params.maxKeyLen-params.minKeyLen + 1) + params.minKeyLen;
-    std::string nextKey(currKeyLen, 0);
-    for(size_t j=0; j<currKeyLen; ++j) {
-      nextKey[j] = (rand() % 26) + 'a';
-    }
-    if(!keysSet.count(nextKey)) {
-      keysSet.insert(nextKey);
-    }
-  }
-
-  std::vector<std::string> keys(
-        std::make_move_iterator(keysSet.begin()),
-        std::make_move_iterator(keysSet.end())
-    );
-
-  std::vector<std::string> keysSorted(keys);
-
-  std::sort(keysSorted.begin(), keysSorted.end(), [](auto& l, auto& r){
-    return l.size() < r.size();
-  });
-
-  // Stores indices of all keys to be put into the text
-  std::vector<size_t> textVecOfKeys;
-  
-  size_t textCharsReplacedWithKeys = 0;
-  size_t targetTextCharsReplacedWithKeys = ((double) params.keyFrequency / (double) 100.0) * params.textLen;
-  targetTextCharsReplacedWithKeys = std::min(targetTextCharsReplacedWithKeys, params.textLen);
-  size_t lastViableKey = keysSorted.size();
-
-  // Replace some of text with keys to generate matches
-  while(textCharsReplacedWithKeys < targetTextCharsReplacedWithKeys && lastViableKey > 0) {
-    size_t nextKeyInd = rand()%lastViableKey;
-    std::string& nextKey = keysSorted[nextKeyInd];
-    if(nextKey.size() + textCharsReplacedWithKeys > targetTextCharsReplacedWithKeys) {
-      lastViableKey = nextKeyInd;
-      continue;
-    }
-    textVecOfKeys.push_back(nextKeyInd);
-    textCharsReplacedWithKeys += nextKey.size();
-  }
-
-  // Ensure that there is at least one key if the key frequency is non-zero
-  // Covers edge case where all keys are too long to fit without going over desired frequency,
-  // otherwise causing zero matches at low key frequencies
-  if((textCharsReplacedWithKeys == 0 && targetTextCharsReplacedWithKeys > 0)
-  && (keysSorted[0].size() < params.textLen)) {
-    textVecOfKeys.push_back(0);
-    textCharsReplacedWithKeys = keysSorted[0].size();
-  }
-
-  // Generate random string of text of length params.textLen, all of uppercase letters so that there aren't extra matches
-  std::string text(params.textLen, 0);
-
-  #pragma omp parallel
-  {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist('A', 'Z');
-
-    #pragma omp for
-    for (size_t i = 0; i < params.textLen; ++i) {
-      text[i] = dist(gen);
-    }
-  }
-
-  std::random_device global_rd;
-  std::mt19937 global_gen(global_rd());
-  
-  // Replace text with keys, approximately evenly spaced
-  if(textVecOfKeys.size() == 1) {
-    hammingTextReplace(text, 0, keysSorted[0], params.maxHammingDistance, global_gen);
-  } else if(textVecOfKeys.size() > 1) {
-    size_t nonKeyCharsInText = params.textLen - textCharsReplacedWithKeys;
-    size_t minSpace = nonKeyCharsInText / textVecOfKeys.size();
-    size_t extraSpaces = nonKeyCharsInText % textVecOfKeys.size();
-
-    size_t textInd = 0;
-    for(size_t i=0; i < textVecOfKeys.size(); ++i) {
-      std::string& currentKey = keysSorted[textVecOfKeys[i]];
-      hammingTextReplace(text, textInd, currentKey, params.maxHammingDistance, global_gen);
-      textInd += currentKey.size();
-      textInd += minSpace;
-      if(extraSpaces > 0) {
-        ++textInd;
-        -- extraSpaces;
-      }
-    }
-  }
-  
-  std::string outputFile(params.outputFile);
-  std::string outputDir = "./../dataset/" + outputFile;
-  std::string keyOutputFile = outputDir + "/keys.txt";
-  std::string textOutputFile = outputDir + "/text.txt";
-  std::string hammingDistanceOutputFile = outputDir + "/maxHammingDistance.txt";
-
-  if (!std::filesystem::create_directory(outputDir)) {
-    std::cerr << "Error creating output directory, dataset/" << params.outputFile << " may already exist" << std::endl;
-    return 1;
-  }
-
-  FILE* textFile = fopen(textOutputFile.c_str(), "w");
-  if (textFile == nullptr) {
-    std::cerr << "Error opening text file" << std::endl;
-    return 1;
-  }
-
-  size_t textWritten = fwrite(text.c_str(), sizeof(char), text.size(), textFile);
-  if (textWritten != text.size()) {
-    std::cerr << "Error writing to text file" << std::endl;
-    return 1;
-  }
-
-  fclose(textFile);
-
-  FILE* keysFile = fopen(keyOutputFile.c_str(), "w");
-  if (keysFile == nullptr) {
-    std::cerr << "Error opening keys file" << std::endl;
-    return 1;
-  }
-
-  const std::string newlineChar = "\n";
-  for(size_t i=0; i<keys.size(); ++i) {
-    size_t keyWritten = fwrite(keys[i].c_str(), sizeof(char), keys[i].size(), keysFile);
-    if (keyWritten != keys[i].size()) {
-      std::cerr << "Error writing to keys file" << std::endl;
-      return 1;
-    }
-    fwrite(newlineChar.c_str(), sizeof(char), newlineChar.size(), keysFile);
-  }
-
-  fclose(keysFile);
-
-  std::string hammingDistanceString = std::to_string(params.maxHammingDistance);
-
-  FILE* hammingDistanceFile = fopen(hammingDistanceOutputFile.c_str(), "w");
-  if (hammingDistanceFile == nullptr) {
-    std::cerr << "Error opening hamming distance file" << std::endl;
-    return 1;
-  }
-
-  size_t hammingDistanceWritten = fwrite(hammingDistanceString.c_str(), sizeof(char), hammingDistanceString.size(), hammingDistanceFile);
-  if (hammingDistanceWritten != hammingDistanceString.size()) {
-    std::cerr << "Error writing to hamming distance file" << std::endl;
-    return 1;
-  }
-
-  fclose(hammingDistanceFile);
-
-  std::cout << "Successfully wrote to dataset/" << params.outputFile << "/keys.txt, dataset/"
-            << params.outputFile << "/text.txt, and dataset/"
-            << params.outputFile << "/maxHammingDistance.txt" << std::endl;
+  generateStringMatchData(params.outputFile, params.textLen,
+    params.numKeys, params.minKeyLen,
+    params.maxKeyLen, params.keyFrequency,
+    true, params.maxHammingDistance);
   
   return 0;
 }
--- util/utilStringMatch.h
+++ util/utilStringMatch.h
@@ -13,6 +13,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdint.h>
+#include <unordered_set>
+#include <filesystem>
 #include <algorithm>                   // for max
 #include <limits>                      // for numeric_limits
 
@@ -114,6 +116,235 @@ void hammingStringMatchCpu(const std::vector<std::string>& needles, const std::s
         matches[haystackIdx] = std::max(matches[haystackIdx], static_cast<int>(1 + needleIdx));
       }
     }
+  }
+}
+
+// Data Generator Functions ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//! @brief  Replaces part of a text with random characters
+//! @param[in,out]  text  The text to replace part of
+//! @param[in]  idx  The start index to replace from
+//! @param[in]  key  The key to insert into the text
+//! @param[in]  maxHammingDistance  The maximum number of character changes to the key before insertion
+//! @param[in]  gen  Random number generator
+void hammingTextReplace(std::string& text, const uint64_t idx, const std::string& key, const uint64_t maxHammingDistance, std::mt19937& gen) {
+  std::uniform_int_distribution<> numCharsDist(0, std::min(key.size(), maxHammingDistance));
+  uint64_t numCharsToSwitch = numCharsDist(gen);
+  std::uniform_int_distribution<> charsDist('a', 'b');
+  for(uint64_t keyIdx=0; keyIdx < key.size(); ++keyIdx) {
+    char nextChar;
+    if(numCharsToSwitch) {
+      nextChar = charsDist(gen);
+      --numCharsToSwitch;
+    } else {
+      nextChar = key[keyIdx];
+    }
+    text[idx + keyIdx] = nextChar;
+  }
+}
+
+//! @brief  Generates test data for string matching (Hamming or exact)
+//! @param[in]  outputFolderName  output folder name, stores output in dataset/[name]/text.txt, dataset/[name]/keys.txt, and, if a hamming distance is provided, dataset/[name]/maxHammingDistance.txt
+//! @param[in]  textLen  The length of text to match against
+//! @param[in]  numKeys  The number of keys to generate
+//! @param[in]  minKeyLen  Minimum key length to generate
+//! @param[in]  maxKeyLen  Maximum key length to generate
+//! @param[in]  keyFrequency  Approximate frequency of keys in generated text, 0=no matches, 100=all keys
+//! @param[in]  isHamming  Whether or not to generate Hamming data
+//! @param[in]  maxHammingDistance  The maximum Hamming distance that matches can have
+void generateStringMatchData(const char* outputFolderName, const uint64_t textLen,
+                             const uint64_t numKeys, const uint64_t minKeyLen,
+                             const uint64_t maxKeyLen, const uint8_t keyFrequency,
+                             const bool isHamming = false, const uint64_t maxHammingDistance = 0
+                            ) {
+  if(outputFolderName == nullptr) {
+    std::cerr << "Output filename must be provided!" << std::endl;
+    std::exit(1);
+  }
+
+  double maxPossibleKeys = 0;
+  for (size_t length = minKeyLen; length <= maxKeyLen; ++length) {
+    maxPossibleKeys += std::pow(26, length);
+  }
+
+  if((double) numKeys > maxPossibleKeys) {
+    std::cerr << "Error: Number of keys greater than max possible keys for the given length range" << std::endl;
+    std::cerr << "Requested Keys: " << numKeys << ", max number of unique keys of length [" << minKeyLen << ", " << maxKeyLen << "]: " << std::fixed << std::setprecision(0) << maxPossibleKeys << std::endl;
+    std::exit(1);
+  }
+
+  std::unordered_set<std::string> keysSet;
+  keysSet.reserve(numKeys);
+
+  // Create numKeys randomly generated keys of random sizes in range [minKeyLen, maxKeyLen]
+  // All keys are unique
+  while(keysSet.size() < numKeys) {
+    size_t currKeyLen = rand()%(maxKeyLen-minKeyLen + 1) + minKeyLen;
+    std::string nextKey(currKeyLen, 0);
+    for(size_t j=0; j<currKeyLen; ++j) {
+      nextKey[j] = (rand() % 26) + 'a';
+    }
+    if(!keysSet.count(nextKey)) {
+      keysSet.insert(nextKey);
+    }
+  }
+
+  std::vector<std::string> keys(
+        std::make_move_iterator(keysSet.begin()),
+        std::make_move_iterator(keysSet.end())
+    );
+
+  std::vector<std::string> keysSorted(keys);
+
+  std::sort(keysSorted.begin(), keysSorted.end(), [](auto& l, auto& r){
+    return l.size() < r.size();
+  });
+
+  // Stores indices of all keys to be put into the text
+  std::vector<size_t> textVecOfKeys;
+  
+  size_t textCharsReplacedWithKeys = 0;
+  size_t targetTextCharsReplacedWithKeys = ((double) keyFrequency / (double) 100.0) * textLen;
+  targetTextCharsReplacedWithKeys = std::min(targetTextCharsReplacedWithKeys, textLen);
+  size_t lastViableKey = keysSorted.size();
+
+  // Replace some of text with keys to generate matches
+  while(textCharsReplacedWithKeys < targetTextCharsReplacedWithKeys && lastViableKey > 0) {
+    size_t nextKeyInd = rand()%lastViableKey;
+    std::string& nextKey = keysSorted[nextKeyInd];
+    if(nextKey.size() + textCharsReplacedWithKeys > targetTextCharsReplacedWithKeys) {
+      lastViableKey = nextKeyInd;
+      continue;
+    }
+    textVecOfKeys.push_back(nextKeyInd);
+    textCharsReplacedWithKeys += nextKey.size();
+  }
+
+  // Ensure that there is at least one key if the key frequency is non-zero
+  // Covers edge case where all keys are too long to fit without going over desired frequency,
+  // otherwise causing zero matches at low key frequencies
+  if((textCharsReplacedWithKeys == 0 && targetTextCharsReplacedWithKeys > 0)
+  && (keysSorted[0].size() < textLen)) {
+    textVecOfKeys.push_back(0);
+    textCharsReplacedWithKeys = keysSorted[0].size();
+  }
+
+  // Generate random string of text of length params.textLen, all of uppercase letters so that there aren't extra matches
+  std::string text(textLen, 0);
+
+  #pragma omp parallel
+  {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist('A', 'Z');
+
+    #pragma omp for
+    for (size_t i = 0; i < textLen; ++i) {
+      text[i] = dist(gen);
+    }
+  }
+
+  std::random_device global_rd;
+  std::mt19937 global_gen(global_rd());
+  
+  // Replace text with keys, approximately evenly spaced
+  if(textVecOfKeys.size() == 1 && keysSorted[0].size() <= text.size()) {
+    if(isHamming) {
+      hammingTextReplace(text, 0, keysSorted[0], maxHammingDistance, global_gen);
+    } else {
+      text.replace(0, keys[0].size(), keys[0]);
+    }
+  } else if(textVecOfKeys.size() > 1) {
+    size_t nonKeyCharsInText = textLen - textCharsReplacedWithKeys;
+    size_t minSpace = nonKeyCharsInText / textVecOfKeys.size();
+    size_t extraSpaces = nonKeyCharsInText % textVecOfKeys.size();
+
+    size_t textInd = 0;
+    for(size_t i=0; i < textVecOfKeys.size(); ++i) {
+      std::string& currentKey = keysSorted[textVecOfKeys[i]];
+      if(isHamming) {
+        hammingTextReplace(text, textInd, currentKey, maxHammingDistance, global_gen);
+      } else {
+        text.replace(textInd, currentKey.size(), currentKey);
+      }
+      textInd += currentKey.size();
+      textInd += minSpace;
+      if(extraSpaces > 0) {
+        ++textInd;
+        -- extraSpaces;
+      }
+    }
+  }
+  
+  std::string outputFileString(outputFolderName);
+  std::string outputDir = "./../dataset/" + outputFileString;
+  std::string keyOutputFile = outputDir + "/keys.txt";
+  std::string textOutputFile = outputDir + "/text.txt";
+
+  if (!std::filesystem::create_directory(outputDir)) {
+    std::cerr << "Error creating output directory, dataset/" << outputFolderName << " may already exist" << std::endl;
+    std::exit(1);
+  }
+
+  FILE* textFile = fopen(textOutputFile.c_str(), "w");
+  if (textFile == nullptr) {
+    std::cerr << "Error opening text file" << std::endl;
+    std::exit(1);
+  }
+
+  size_t textWritten = fwrite(text.c_str(), sizeof(char), text.size(), textFile);
+  if (textWritten != text.size()) {
+    std::cerr << "Error writing to text file" << std::endl;
+    std::exit(1);
+  }
+
+  fclose(textFile);
+
+  FILE* keysFile = fopen(keyOutputFile.c_str(), "w");
+  if (keysFile == nullptr) {
+    std::cerr << "Error opening keys file" << std::endl;
+    std::exit(1);
+  }
+
+  // Regular string matching requires keys to be in sorted order
+  const std::vector<std::string>& keysToWrite = isHamming ? keys : keysSorted;
+
+  const std::string newlineChar = "\n";
+  for(size_t i=0; i<keysToWrite.size(); ++i) {
+    size_t keyWritten = fwrite(keysToWrite[i].c_str(), sizeof(char), keysToWrite[i].size(), keysFile);
+    if (keyWritten != keysToWrite[i].size()) {
+      std::cerr << "Error writing to keys file" << std::endl;
+      std::exit(1);
+    }
+    fwrite(newlineChar.c_str(), sizeof(char), newlineChar.size(), keysFile);
+  }
+
+  fclose(keysFile);
+
+  if(isHamming) {
+    std::string hammingDistanceOutputFile = outputDir + "/maxHammingDistance.txt";
+    std::string hammingDistanceString = std::to_string(maxHammingDistance);
+
+    FILE* hammingDistanceFile = fopen(hammingDistanceOutputFile.c_str(), "w");
+    if (hammingDistanceFile == nullptr) {
+      std::cerr << "Error opening hamming distance file" << std::endl;
+      std::exit(1);
+    }
+
+    size_t hammingDistanceWritten = fwrite(hammingDistanceString.c_str(), sizeof(char), hammingDistanceString.size(), hammingDistanceFile);
+    if (hammingDistanceWritten != hammingDistanceString.size()) {
+      std::cerr << "Error writing to hamming distance file" << std::endl;
+      std::exit(1);
+    }
+
+    fclose(hammingDistanceFile);
+
+    std::cout << "Successfully wrote to dataset/" << outputFolderName << "/keys.txt, dataset/"
+            << outputFolderName << "/text.txt, and dataset/"
+            << outputFolderName << "/maxHammingDistance.txt" << std::endl;
+  } else {
+    std::cout << "Successfully wrote to dataset/" << outputFolderName << "/keys.txt and dataset/"
+            << outputFolderName << "/text.txt" << std::endl;
   }
 }
 
