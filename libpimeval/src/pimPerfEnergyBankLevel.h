c79c87524bf86a14e00ad0db998f5a89a51acfa8
fasiddique <farzana@virginia.edu>
2025-04-11 12:01:31 -0400

    Update util file to fix make error for CPU code

--- libpimeval/src/pimPerfEnergyBankLevel.h
+++ libpimeval/src/pimPerfEnergyBankLevel.h
@@ -29,7 +29,8 @@ public:
   virtual pimeval::perfEnergy getPerfEnergyForRotate(PimCmdEnum cmdType, const pimObjInfo& obj) const override;
 
 protected:
-  double m_blimpCoreLatency = 0.000005 > m_tGDL ? 0.000005 : m_tGDL; // ms; 200 MHz. Reference: BLIMP paper
+  double m_blimpCoreOriginalLatency = 0.000005; // ms; 200 MHz. Reference: BLIMP paper
+  double m_blimpCoreLatency = m_blimpCoreOriginalLatency > m_tGDL ? m_blimpCoreOriginalLatency : m_tGDL; // if compute unit is faster than GDL, compute unit needs to sit idly for next data 
   unsigned m_blimpCoreBitWidth = m_GDLWidth;
   unsigned m_simdUnitCount = m_blimpCoreBitWidth / 32; // 32-bit SIMD unit
   // Following values are taken from fulcrum paper as BLIMP paper does not model energy
--- util/utilBaselines.h
+++ util/utilBaselines.h
@@ -11,8 +11,6 @@
 #include <functional>
 #include <chrono>
 #include <tuple>
-#include <nvml.h>
-#include <cuda_runtime.h>
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
@@ -22,81 +20,9 @@ using namespace std;
 #define WARMUP 2
 #define MAX_NUMBER 102
 
-/**
- * @brief Initializes a vector with random values.
- *
- * This function creates a vector of the specified size and fills it with random
- * values ranging from 0 to MAX_NUMBER. It uses parallelism to speed up the initialization.
- *
- * @param vectorSize The size of the vector to be created.
- * @param dataVector A reference to the vector that will be initialized.
- */
-template <typename T>
-void getVector(uint64_t vectorSize, std::vector<T>& dataVector) {
-    // Resize the vector to the specified size
-    dataVector.resize(vectorSize);
-
-    #pragma omp parallel for
-    for (uint64_t i = 0; i < vectorSize; ++i) {
-        dataVector[i] = static_cast<T>(i % MAX_NUMBER);
-    }
-}
-
-/**
- * @brief Initializes a matrix with random values.
- *
- * This function creates a matrix with the specified number of rows and columns
- * and fills it with random values ranging from 0 to MAX_NUMBER. It uses parallelism
- * to speed up the initialization.
- *
- * @param numRows The number of rows in the matrix.
- * @param numCols The number of columns in the matrix.
- * @param matrix A reference to the matrix that will be initialized.
- */
-template <typename T>
-void getMatrix(uint64_t numRows, uint64_t numCols, vector<vector<T>> &matrix) {
-
-    // Resize the matrix to the specified number of rows and columns
-    matrix.resize(numRows, vector<T>(numCols));
-
-    #pragma omp parallel for
-    for (uint64_t row = 0; row < numRows; ++row) {
-        for (uint64_t col = 0; col < numCols; ++col) {
-            matrix[row][col] = static_cast<T>((row + 1) % MAX_NUMBER);
-        }
-    }
-}
-
-/**
- * @brief Initializes a matrix with random bool values.
- *
- * This function creates a matrix with the specified number of rows and columns
- * and fills it with random values ranging from true to false. It uses parallelism
- * to speed up the initialization. The matrix is stored values are stored as uint8_t
- * because the PIM API expects std::vector<uint8_t> instead of std::vector<bool>.
- *
- * @param numRows The number of rows in the matrix.
- * @param numCols The number of columns in the matrix.
- * @param matrix A reference to the matrix that will be initialized.
- */
-void getMatrixBool(uint64_t numRows, uint64_t numCols, std::vector<std::vector<uint8_t>> &matrix)
-{
-  matrix.resize(numRows, std::vector<uint8_t>(numCols, 0));
-#pragma omp parallel
-  {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint8_t> dist(0, 1);
-
-    #pragma omp for collapse(2)
-    for(size_t i = 0; i < numRows; ++i) {
-      for(size_t j = 0; j < numCols; ++j) {
-        matrix[i][j] = static_cast<bool>(dist(gen));
-      }
-    }
-  }
-}
-
+#if defined(ENABLE_CUDA)
+#include <cuda_runtime.h>
+#include <nvml.h>
 std::tuple<double, double, double> measureCUDAPowerAndElapsedTime(std::function<void()> kernelLaunch)
 {
     nvmlReturn_t result;
@@ -171,6 +97,82 @@ std::tuple<double, double, double> measureCUDAPowerAndElapsedTime(std::function<
     double energy = avgPower * (elapsedTimeMs / 1000.0); // mW × s = mJ
 
     return { elapsedTimeMs, avgPower, energy };
+}
+#endif
+
+/**
+ * @brief Initializes a vector with random values.
+ *
+ * This function creates a vector of the specified size and fills it with random
+ * values ranging from 0 to MAX_NUMBER. It uses parallelism to speed up the initialization.
+ *
+ * @param vectorSize The size of the vector to be created.
+ * @param dataVector A reference to the vector that will be initialized.
+ */
+template <typename T>
+void getVector(uint64_t vectorSize, std::vector<T>& dataVector) {
+    // Resize the vector to the specified size
+    dataVector.resize(vectorSize);
+
+    #pragma omp parallel for
+    for (uint64_t i = 0; i < vectorSize; ++i) {
+        dataVector[i] = static_cast<T>(i % MAX_NUMBER);
+    }
+}
+
+/**
+ * @brief Initializes a matrix with random values.
+ *
+ * This function creates a matrix with the specified number of rows and columns
+ * and fills it with random values ranging from 0 to MAX_NUMBER. It uses parallelism
+ * to speed up the initialization.
+ *
+ * @param numRows The number of rows in the matrix.
+ * @param numCols The number of columns in the matrix.
+ * @param matrix A reference to the matrix that will be initialized.
+ */
+template <typename T>
+void getMatrix(uint64_t numRows, uint64_t numCols, vector<vector<T>> &matrix) {
+
+    // Resize the matrix to the specified number of rows and columns
+    matrix.resize(numRows, vector<T>(numCols));
+
+    #pragma omp parallel for
+    for (uint64_t row = 0; row < numRows; ++row) {
+        for (uint64_t col = 0; col < numCols; ++col) {
+            matrix[row][col] = static_cast<T>((row + 1) % MAX_NUMBER);
+        }
+    }
+}
+
+/**
+ * @brief Initializes a matrix with random bool values.
+ *
+ * This function creates a matrix with the specified number of rows and columns
+ * and fills it with random values ranging from true to false. It uses parallelism
+ * to speed up the initialization. The matrix is stored values are stored as uint8_t
+ * because the PIM API expects std::vector<uint8_t> instead of std::vector<bool>.
+ *
+ * @param numRows The number of rows in the matrix.
+ * @param numCols The number of columns in the matrix.
+ * @param matrix A reference to the matrix that will be initialized.
+ */
+void getMatrixBool(uint64_t numRows, uint64_t numCols, std::vector<std::vector<uint8_t>> &matrix)
+{
+  matrix.resize(numRows, std::vector<uint8_t>(numCols, 0));
+#pragma omp parallel
+  {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint8_t> dist(0, 1);
+
+    #pragma omp for collapse(2)
+    for(size_t i = 0; i < numRows; ++i) {
+      for(size_t j = 0; j < numCols; ++j) {
+        matrix[i][j] = static_cast<bool>(dist(gen));
+      }
+    }
+  }
 }
 
 #endif // _COMMON_H_
