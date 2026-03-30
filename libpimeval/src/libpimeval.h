9a78b485d4c7d4b8924a5b0a1d7985c63beef2d3
Farzana Ahmed Siddique <fas9nw@virginia.edu>
2026-03-30 13:46:39 -0400

    Apply suggestion from @fasiddique

--- libpimeval/src/libpimeval.h
+++ libpimeval/src/libpimeval.h
@@ -169,7 +169,7 @@ PimStatus pimConvertType(PimObjId src, PimObjId dest);
 // Logic and Arithmetic Operation
 // Note: idxBegin and idxEnd specify the range [idxBegin, idxEnd) of indexes to be processed by the PIM.
 // If the default values (OULL) for idxBegin and idxEnd are used, the entire range of the PIM object will be considered.
-// TODO: Currently idxBegin and idxEnd for all PIM Object IDs are same; however, it is possible to perform operation on  
+// TODO: Currently idxBegin and idxEnd for all PIM Object IDs are same; however, it is possible to perform operation on  same range  of different objects but different idxBegin and idxEnd
 // Mixed data type extensions:
 // - pimAdd/pimSub: If src1 is an integer vector, src2 can be a Boolean vector for accumulation purposes.
 PimStatus pimAdd(PimObjId src1, PimObjId src2, PimObjId dest, uint64_t idxBegin = 0, uint64_t idxEnd = 0, PimIndexMode indexMode = PIM_GLOBAL);
